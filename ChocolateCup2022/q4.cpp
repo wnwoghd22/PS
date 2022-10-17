f654d3dc2e5d436b0a7fc385c1d971a4f75f2298
wnwoghd22 <53481297+wnwoghd22@users.noreply.github.com>
2022-10-17 16:15:11 +0900

    Update q4.cpp
    코드는 짧게,
    공식은 우아하게.

--- ChocolateCup2022/q4.cpp
+++ ChocolateCup2022/q4.cpp
@@ -4,14 +4,12 @@ typedef long long int ll;
 const int LEN = 100'001;
 const ll MOD = 1'000'000'007;
 
-ll dp[LEN][3];
+ll dp[LEN][2];
 
 int main() {
-	dp[0][0] = 1;
-	for (int i = 1; i < LEN; ++i) {
-		if (i & 1) dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % MOD;
-		else dp[i][0] = (dp[i - 1][1] * 2 + dp[i - 2][0]) % MOD, dp[i][2] = dp[i - 1][1];
-	}
+	dp[0][0] = 1, dp[1][1] = 1;
+	for (int i = 2, j = 0; i < LEN; ++i, j = !j)
+		dp[i][j] = ((dp[i - 1][!j] << !j) + dp[i - 2][j]) % MOD;
 	ll T, N, R, C;
 
 	std::ios_base::sync_with_stdio(0);
@@ -20,7 +18,7 @@ int main() {
 	std::cin >> T;
 	while (T--) {
 		std::cin >> N >> R >> C;
-		if (R & 1) std::cout << (dp[C][1] * dp[N - C][0] % MOD + dp[N - C + 1][1] * dp[C - 1][0] % MOD - dp[C - 1][0] * dp[N - C][0] % MOD + MOD) % MOD << '\n';
-		else std::cout << (dp[C][2] * dp[N - C + 1][2] * 2) % MOD << '\n';
+		if (R & 1) std::cout << (dp[C][1] * dp[N - C][0] + dp[N - C + 1][1] * dp[C - 1][0] - dp[C - 1][0] * dp[N - C][0] % MOD + MOD) % MOD << '\n';
+		else std::cout << (dp[C - 1][1] * dp[N - C][1] * 2) % MOD << '\n';
 	}
 }

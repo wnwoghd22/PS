e3296aca943c18958a30568a8a5d7965768d8ad1
wnwoghd22 <53481297+wnwoghd22@users.noreply.github.com>
2022-10-20 15:29:03 +0900

    update

--- q15678.cpp
+++ q15678.cpp
@@ -2,24 +2,20 @@
 #include <queue>
 
 typedef long long int ll;
-const int LEN = 100'001;
 const ll INF = 1e15;
-ll N, K, A[LEN], dp[LEN], max = -INF;
+ll N, D, K, S, M = -INF;
 std::deque<std::pair<ll, int>> dq;
 
 int main() {
-	std::cin >> N >> K;
-	for (int i = 1; i <= N; ++i) std::cin >> A[i], dp[i] = A[i];
-
+	std::cin >> N >> D;
 	dq.push_back({ 0, 0 });
 	for (int i = 1; i <= N; ++i) {
-		while (!dq.empty() && dq.front().second < i - K) dq.pop_front();
-		dp[i] = std::max(dp[i], A[i] + dq.front().first);
-		max = std::max(dp[i], max);
-		std::cout << dp[i] << ' ' << A[i] << ' ' << dq.front().first << '\n';
-		while (!dq.empty() && dq.back().first < dp[i]) dq.pop_back();
-		dq.push_back({ dp[i], i });
+		std::cin >> K;
+		while (!dq.empty() && dq.front().second < i - D) dq.pop_front();
+		S = std::max(K, K + dq.front().first);
+		M = std::max(S, M);
+		while (!dq.empty() && dq.back().first < S) dq.pop_back();
+		dq.push_back({ S, i });
 	}
-
-	std::cout << max;
+	std::cout << M;
 }
--- q5977.cpp
+++ q5977.cpp
@@ -1,21 +1,39 @@
 #include <iostream>
 #include <queue>
 
+//typedef long long int ll;
+//const int LEN = 100'001;
+//ll N, K, A[LEN], S[LEN], dp[LEN];
+//std::deque<std::pair<ll, int>> dq;
+//
+//int main() {
+//	std::cin >> N >> K;
+//	for (int i = 1; i <= N; ++i) std::cin >> A[i], S[i] = A[i] + S[i - 1];
+//	dq.push_back({ 0, 0 });
+//	for (int i = 1; i <= N; ++i) {
+//		while (!dq.empty() && dq.front().second < i - K) dq.pop_front();
+//		dp[i] = S[i] + dq.front().first;
+//		while (!dq.empty() && dq.back().first < dp[i - 1] - S[i]) dq.pop_back();
+//		dq.push_back({ dp[i - 1] - S[i], i });
+//		dp[i] = std::max(dp[i], dp[i - 1]);
+//	}
+//	std::cout << dp[N];
+//}
+
 typedef long long int ll;
-const int LEN = 100'001;
-ll N, K, A[LEN], S[LEN], dp[LEN];
+ll N, K, E, S, s;
 std::deque<std::pair<ll, int>> dq;
 
 int main() {
-	std::cin >> N >> K;
-	for (int i = 1; i <= N; ++i) std::cin >> A[i], S[i] = A[i] + S[i - 1];
+	std::cin >> N >> K; K++;
 	dq.push_back({ 0, 0 });
 	for (int i = 1; i <= N; ++i) {
+		std::cin >> E, S += E;
 		while (!dq.empty() && dq.front().second < i - K) dq.pop_front();
-		dp[i] = S[i] + dq.front().first;
-		while (!dq.empty() && dq.back().first < dp[i - 1] - S[i]) dq.pop_back();
-		dq.push_back({ dp[i - 1] - S[i], i });
-		dp[i] = std::max(dp[i], dp[i - 1]);
+		s = E + dq.front().first;
+		while (!dq.empty() && dq.back().first > s) dq.pop_back();
+		dq.push_back({ s, i });
 	}
-	std::cout << dp[N];
+	while (!dq.empty() && dq.front().second <= N - K) dq.pop_front();
+	std::cout << S - dq.front().first;
 }

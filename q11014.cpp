4ce9137824f2aa99c5b85ca0985505a9e51b9111
wnwoghd22 <53481297+wnwoghd22@users.noreply.github.com>
2022-11-09 07:54:51 +0900

    update
    M이 홀수일 때 좌표 압축이 유효하지 않다.

--- q11014.cpp
+++ q11014.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <algorithm>
 
-const int LEN = 3201;
+const int LEN = 6401;
 
 std::string S[80];
 std::vector<int> a[LEN];
@@ -14,7 +14,7 @@ bool dfs(int x) {
 	for (const int& t : a[x]) {
 		if (c[t]) continue;
 		c[t] = true;
-		if (!d[t] || dfs(d[t])) {
+		if (!~d[t] || dfs(d[t])) {
 			d[t] = x;
 			return true;
 		}
@@ -27,7 +27,7 @@ int main() {
 	std::cin >> T;
 	while (T--) {
 		for (std::vector<int>& v : a) v.clear();
-		std::fill(d, d + LEN, 0);
+		std::fill(d, d + LEN, -1);
 
 		int total = 0, count = 0;
 		std::cin >> N >> M;
@@ -40,25 +40,28 @@ int main() {
 			for (int j = 0; j < M; j += 2) {
 				if (S[i][j] == 'x') continue;
 				if (i > 0 && j > 0 && S[i - 1][j - 1] == '.')
-					a[(i * M + j) / 2 + 1].push_back(((i - 1) * M + j - 1) / 2 + 1);
+					a[i * M + j].push_back((i - 1) * M + j - 1);
 				if (i > 0 && j < M - 1 && S[i - 1][j + 1] == '.')
-					a[(i * M + j) / 2 + 1].push_back(((i - 1) * M + j + 1) / 2 + 1);
+					a[i * M + j].push_back((i - 1) * M + j + 1);
 
 				if (j > 0 && S[i][j - 1] == '.')
-					a[(i * M + j) / 2 + 1].push_back((i * M + j - 1) / 2 + 1);
+					a[i * M + j].push_back(i * M + j - 1);
 				if (j < M - 1 && S[i][j + 1] == '.')
-					a[(i * M + j) / 2 + 1].push_back((i * M + j + 1) / 2 + 1);
+					a[i * M + j].push_back(i * M + j + 1);
 
 				if (i < N - 1 && j > 0 && S[i + 1][j - 1] == '.')
-					a[(i * M + j) / 2 + 1].push_back(((i + 1) * M + j - 1) / 2 + 1);
+					a[i * M + j].push_back((i + 1) * M + j - 1);
 				if (i < N - 1 && j < M - 1 && S[i + 1][j + 1] == '.')
-					a[(i * M + j) / 2 + 1].push_back(((i + 1) * M + j + 1) / 2 + 1);
+					a[i * M + j].push_back((i + 1) * M + j + 1);
 			}
 		}
 
-		for (int i = 1; i <= N * M / 2; ++i) {
-			std::fill(c, c + N * M / 2 + 1, false);
-			if (dfs(i)) ++count;
+		for (int i = 0; i < N; ++i) {
+			for (int j = 0; j < M; j += 2) {
+				std::fill(c, c + LEN, false);
+				if (dfs(i * M + j)) ++count;
+
+			}
 		}
 		std::cout << total - count << '\n';
 	}

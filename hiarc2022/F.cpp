01cdb7d1a5a1888144c2b05d16df72214827494f
wnwoghd22 <53481297+wnwoghd22@users.noreply.github.com>
2022-11-20 17:25:05 +0900

    Update F.cpp
    우선순위 큐로 어찌어찌 통과는 되는듯.

--- hiarc2022/F.cpp
+++ hiarc2022/F.cpp
@@ -4,34 +4,36 @@
 #include <algorithm>
 
 typedef std::pair<int, int> pii;
+typedef std::pair<int, pii> ipi;
+typedef std::pair<pii, int> ppi;
 
 int map[3000][3000];
 int N, M, K, r, c, d;
-std::queue<std::pair<pii, int>> qj;
+std::priority_queue<ipi> pq;
 const int dx[4] = { -1, 1, 0, 0 }, dy[4] = { 0, 0, -1, 1 };
 
 void bfs_jam() {
-	while (!qj.empty()) {
-		const auto& pos = qj.front(); qj.pop();
-		if (pos.second == 1) continue;
-		int jam = pos.second - 1;
+	while (!pq.empty()) {
+		ipi pos = pq.top(); pq.pop();
+		if (pos.first == 1) continue;
+		int jam = pos.first - 1;
 		for (int i = 0; i < 4; ++i) {
-			int dr = pos.first.first + dx[i], dc = pos.first.second + dy[i];
+			int dr = pos.second.first + dx[i], dc = pos.second.second + dy[i];
 			if (dr < 0 || dr >= N || dc < 0 || dc >= M) continue;
 			if (jam > map[dr][dc]) {
 				map[dr][dc] = jam;
-				if (jam > 1) qj.push({ {dr, dc}, jam });
+				if (jam > 1) pq.push({ jam, {dr, dc} });
 			}
 		}
 	}
 }
 
 int bfs() {
-	std::queue<std::pair<pii, int>> q;
+	std::queue<ppi> q;
 	q.push({ {0, 0}, 0 });
 	map[0][0] = 1;
 	while (!q.empty()) {
-		const auto& pos = q.front(); q.pop();
+		ppi pos = q.front(); q.pop();
 		if (pos.first.first == N - 1 && pos.first.second == M - 1) return pos.second;
 		for (int i = 0; i < 4; ++i) {
 			int dr = pos.first.first + dx[i], dc = pos.first.second + dy[i];
@@ -53,9 +55,16 @@ int main() {
 		std::cin >> r >> c >> d;
 		map[r - 1][c - 1] = d + 1;
 		if (d == 0) continue;
-		qj.push({ {r - 1, c - 1}, d + 1 });
+		pq.push({d + 1, {r - 1, c - 1}});
 	}
 	bfs_jam();
+
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < M; ++j)
+			std::cout << map[i][j] << ' ';
+		std::cout << '\n';
+	}
+
 	int result = bfs();
 
 	if (~result) std::cout << "YES\n" << result;

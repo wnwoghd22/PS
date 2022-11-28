58755935f9a5f4b4698bc2e1ddac499e9fbe519b
wnwoghd22 <53481297+wnwoghd22@users.noreply.github.com>
2022-11-29 07:42:49 +0900

    Update q5615.cpp
    오버플로우 방지가 생각보다 까다롭다.

--- q5615.cpp
+++ q5615.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-typedef long long int ll;
+typedef unsigned long long int ll;
 
 ll power(ll a, ll b, ll mod) {
     if (b == 1) return a % mod;
@@ -8,6 +8,18 @@ ll power(ll a, ll b, ll mod) {
         ll pw = power(a, b / 2, mod);
         return pw * pw % mod * (b & 1 ? a : 1) % mod;
     }
+}
+
+// prevent overflow
+ll power_(ll x, ll y, ll mod) {
+    ll res = 1;
+    x %= mod;
+    while (y) {
+        if (y & 1) res = (res * x) % mod;
+        y >>= 1;
+        x = (x * x) % mod;
+    }
+    return res;
 }
 
 bool miller_rabin(const ll n, const ll a) {
@@ -21,7 +33,7 @@ bool miller_rabin(const ll n, const ll a) {
     }
 }
 
-bool miller_rabin(const ll n) { 
+bool miller_rabin(const ll n) {
     return miller_rabin(n, 2) && miller_rabin(n, 7) && miller_rabin(n, 61);
 }
 

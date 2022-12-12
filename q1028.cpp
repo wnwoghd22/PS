7ed30c604bb119a587082faf2dc972e98744cb11
wnwoghd22 <53481297+wnwoghd22@users.noreply.github.com>
2022-12-12 23:41:58 +0900

    Update q1028.cpp
    O(N^3) 풀이는 시간초과.
    O(N^2)로 다시 풀어야 했다.

--- q1028.cpp
+++ q1028.cpp
@@ -2,31 +2,49 @@
 #include <iostream>
 #include <algorithm>
 
-bool dp[750][750][750][4];
+const int LEN = 2000;
+int map[LEN][LEN];
+int lu[LEN][LEN], ru[LEN][LEN], ld[LEN][LEN], rd[LEN][LEN];
 int R, C, result;
 
 int main() {
 	scanf("%d %d", &R, &C);
 
-	for (int r = 0; r < R; ++r) {
-		for (int e, c = 0; c < C; ++c) {
-			scanf("%1d", &e);
-			dp[0][r][c][0] = dp[0][r][c][1] = dp[0][r][c][2] = dp[0][r][c][3] = e;
-			if (e) result = 1;
-		}
-	}
+	for (int r = 1; r <= R; ++r)
+		for (int c = 1; c <= C; ++c)
+			scanf("%1d", &map[r][c]);
 
-	for (int i = 1; i <= std::min(R, C) / 2; ++i) {
-		for (int r = i; r < R - i; ++r) {
-			for (int c = i; c < C - i; ++c) {
-				if (dp[i - 1][r - 1][c][0] && dp[i - 1][r][c - 1][0]) dp[i][r][c][0] = 1;
-				if (dp[i - 1][r - 1][c][1] && dp[i - 1][r][c + 1][1]) dp[i][r][c][1] = 1;
-				if (dp[i - 1][r + 1][c][2] && dp[i - 1][r][c - 1][2]) dp[i][r][c][2] = 1;
-				if (dp[i - 1][r + 1][c][3] && dp[i - 1][r][c + 1][3]) dp[i][r][c][3] = 1;
-
-				if (dp[i][r][c][0] && dp[i][r][c][1] && dp[i][r][c][2] && dp[i][r][c][3]) result = i + 1;
+	for (int r = R; r >= 1; --r) {
+		for (int c = 1; c <= C; ++c) {
+			if (map[r][c]) {
+				ld[r][c] = ld[r + 1][c - 1] + 1;
+				rd[r][c] = rd[r + 1][c + 1] + 1;
 			}
 		}
 	}
+	for (int r = 1; r <= R; ++r) {
+		for (int c = 1; c <= C; ++c) {
+			if (map[r][c]) {
+				lu[r][c] = lu[r - 1][c - 1] + 1;
+				ru[r][c] = ru[r - 1][c + 1] + 1;
+			}
+		}
+	}
+	for (int r = 1; r <= R; ++r) {
+		for (int c = 1; c <= C; ++c) {
+			for (int k = 1; k <= std::min(ld[r][c], rd[r][c]); ++k) {
+				int d = r + 2 * (k - 1);
+				if (map[d][c] && std::min(lu[d][c], ru[d][c]) >= k)
+					result = std::max(result, k);
+			}
+			for (int k = 1; k <= std::min(ru[r][c], rd[r][c]); ++k) {
+				int l = c + 2 * (k - 1);
+				if (map[r][l] && std::min(lu[r][l], ld[r][l]) >= k)
+					result = std::max(result, k);
+
+			}
+		}
+	}
+
 	std::cout << result;
 }

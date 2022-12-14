eeb864247d1d47e36e4cc87a3ea47e4ef25f4a53
wnwoghd22 <53481297+wnwoghd22@users.noreply.github.com>
2022-12-14 22:11:54 +0900

    update

---
+++ q3683.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+
+struct Vote { int idx, cat, dog; };
+const int LEN = 501;
+std::vector<int> a[LEN];
+std::vector<Vote> cat;
+std::vector<Vote> dog;
+int d[LEN];
+bool c[LEN];
+int T, C, D, V;
+
+bool dfs(int x) {
+	for (const int& t : a[x]) {
+		if (c[t]) continue;
+		c[t] = true;
+		if (!d[t] || dfs(d[t])) {
+			d[t] = x;
+			return true;
+		}
+	}
+	return false;
+}
+
+void solve() {
+	for (std::vector<int>& v : a) v.clear();
+	cat.clear(); dog.clear();
+	memset(c, 0, sizeof c);
+	memset(d, 0, sizeof d);
+	char pros, cons;
+	int prosI, consI;
+	std::cin >> C >> D >> V;
+	for (int i = 1; i <= V; ++i) {
+		std::cin >> pros >> prosI >> cons >> consI;
+		if (pros == 'C') cat.emplace_back(i, prosI, consI);
+		if (pros == 'D') dog.emplace_back(i, consI, prosI);
+	}
+	for (const Vote& cv : cat) {
+		for (const Vote& dv : dog) {
+			if (cv.cat == dv.cat || cv.dog == dv.dog) {
+				a[cv.idx].push_back(dv.idx);
+			}
+		}
+	}
+
+	int count = 0;
+	for (int i = 1; i <= V; ++i) {
+		std::fill(c, c + V + 1, false);
+		if (dfs(i)) ++count;
+	}
+	std::cout << V - count << '\n';
+}
+
+int main() {
+	std::cin >> T;
+	while (T--) solve();
+}

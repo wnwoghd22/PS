47b76f74493dec469dd1ba991c3a0c39d2e240f9
Jay <wnwoghd22@gmail.com>
2023-01-25 23:25:52 +0900

    solving q13460
    아직 풀이가 완성되지 않았다. 그것보다 이 문제, 어떻게 푸는 거지...?

---
+++ q13460.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+typedef std::pair<int, int> pii;
+const int INF = 1e9;
+char map[11][11];
+int N, M;
+
+
+
+int main() {
+	std::cin >> N >> M;
+	for (int i = 0; i < N; ++i) std::cin >> map[i];
+
+}

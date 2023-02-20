350f9f126e2cb1f9114b510576a44037cbab8ad2
Jay <wnwoghd22@gmail.com>
2023-02-20 19:08:27 +0900

    update
    내가 이해했던 것과는 전혀 다른 문제였다. 해석을 잘못 한 것도 있고, 접근도 틀렸었다.

--- q14636.cpp
+++ q14636.cpp
@@ -1,52 +1,52 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <vector>
 #include <algorithm>
 
+typedef long long int ll;
 const int LEN = 500'000;
 struct Company {
 	int t, p;
-	bool operator<(const Company& r) const { return t == r.t ? p > r.p : t < r.t; }
+	bool operator<(const Company& r) const { return t == r.t ? p < r.p : t < r.t; }
 } seller[LEN], buyer[LEN];
-int N, M, bound[LEN];
+std::vector<Company> S, B;
+int N, M;
 
-int binary_search(int e) { // get upper bound
-	int l = 0, r = M, m;
-	while (l < r) {
-		m = l + r >> 1;
-		if (seller[m].t <= e) l = m + 1;
-		else r = m;
-	}
-	return r;
+ll c(int i, int j) {
+	ll dx = B[j].p - S[i].p;
+	ll dy = B[j].t - S[i].t;
+	return dx < 0 && dy < 0 ? 0 : dx * dy;
 }
 
-int c(int i, int j) { return buyer[j].p - seller[i].p; }
-
-int f(int s, int e, int l, int r) {
+ll f(int s, int e, int l, int r) {
 	if (s > e) return 0;
-	int opt = l, benefit = 0;
-	int m = s + e >> 1;
-	// int bound = binary_search(buyer[m].t) - 1;
+	ll benefit;
+	int opt = l, m = s + e >> 1;
 
-	if (l <= bound[m]) {
-		for (int i = opt; i <= std::min(r, bound[m]); ++i) {
-			if (c(i, m) > c(opt, m)) opt = i;
-		}
-		benefit = c(opt, m);
+	for (int i = opt; i <= r; ++i) {
+		if (c(m, i) > c(m, opt)) opt = i;
 	}
+	benefit = c(m, opt);
 
 	return std::max({ benefit, f(s, m - 1, l, opt), f(m + 1, e, opt, r) });
 }
 
 int main() {
-	freopen("input.txt", "r", stdin);
+	// freopen("input.txt", "r", stdin);
 	std::cin >> M >> N;
 	for (int i = 0; i < M; ++i) std::cin >> seller[i].p >> seller[i].t;
 	for (int i = 0; i < N; ++i) std::cin >> buyer[i].p >> buyer[i].t;
 	std::sort(seller, seller + M);
 	std::sort(buyer, buyer + N);
-	for (int i = 0, j = 0; i < N; ++i) {
-		while (j < M && seller[j].t <= buyer[i].t) ++j;
-		bound[i] = j - 1;
+
+	// make monotonic
+	for (int i = 0; i < M; ++i) {
+		if (S.empty() || seller[i].p < S.back().p)
+			S.push_back(seller[i]);
 	}
-	std::cout << f(0, N - 1, 0, M - 1);
+	for (int i = 0; i < N; ++i) {
+		while (B.size() && buyer[i].p >= B.back().p) B.pop_back();
+		B.push_back(buyer[i]);
+	}
+	std::cout << f(0, S.size() - 1, 0, B.size() - 1);
 }

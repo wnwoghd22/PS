2bd2780556c2ce1fa048b6848f41d9fd512d249b
Jay <wnwoghd22@gmail.com>
2023-03-02 18:51:42 +0900

    update
    q13141 ignition: 불필요하거나 비효율적인 부분을 모두 없앴다.
    q5820: 코드 작성 완료. 맞는지는 모르겠다.

--- q13141.cpp
+++ q13141.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <cstring>
 
 const int LEN = 201;
 const int INF = 1e9;
@@ -11,10 +12,11 @@ struct Info {
 	bool operator<(const Info& r) const { return d > r.d; }
 };
 std::vector<Edge> graph[LEN];
-int N, M, S, E, L, dist[LEN];
+int N, M, S, E, L, max_dist[LEN][LEN], min_dist[LEN][LEN], dist[LEN], visited[LEN];
 
 double dijkstra(int s) {
 	std::priority_queue<Info> pq;
+	memset(visited, 0, sizeof visited);
 	for (int i = 1; i <= N; ++i) dist[i] = INF;
 	double result = 0;
 	dist[s] = 0;
@@ -22,17 +24,20 @@ double dijkstra(int s) {
 	while (pq.size()) {
 		auto [u, d] = pq.top(); pq.pop();
 		// std::cout << "dijkstra u: " << u << ", dist: " << d << '\n';
-		if (d > dist[u]) {
-			result = std::max(result, ((double)d + dist[u]) / 2);
-			continue;
-		}
+		if (d > dist[u]) continue;
+		visited[u] = 1;
 		result = std::max(result, (double)d);
 		for (const auto& [v, l] : graph[u]) {
 			if (u == v) {
-				result = std::max(result, d + (double)l / 2);
+				result = std::max(result, d + (double)max_dist[u][u] / 2);
 				continue;
 			}
 			dist[v] = std::min(dist[v], d + l);
+
+			if (visited[v]) {
+				result = std::max(result, ((double)max_dist[u][v] + dist[v] + dist[u]) / 2);
+				continue;
+			}
 			pq.push({ v, d + l });
 		}
 	}
@@ -40,13 +45,19 @@ double dijkstra(int s) {
 }
 
 int main() {
-	freopen("input.txt", "r", stdin);
+	// freopen("input.txt", "r", stdin);
+	std::cin.tie(0)->sync_with_stdio(0);
+
 	std::cin >> N >> M;
 	while (M--) {
 		std::cin >> S >> E >> L;
-		graph[S].push_back({ E, L });
-		graph[E].push_back({ S, L });
+		if (!min_dist[S][E] || L < min_dist[S][E]) min_dist[S][E] = min_dist[E][S] = L;
+		if (L > max_dist[S][E]) max_dist[S][E] = max_dist[E][S] = L;
 	}
+	for (int i = 1; i <= N; ++i)
+		for (int j = 1; j <= N; ++j)
+			if (min_dist[i][j]) graph[i].push_back({ j, min_dist[i][j] });
+
 	double min = 1e9;
 	std::cout << std::fixed;
 	std::cout.precision(1);
--- q5820.cpp
+++ q5820.cpp
@@ -1,14 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <vector>
 #include <map>
+#include <algorithm>
 
 const int INF = 1e9;
 const int LEN = 200'000;
 struct Edge { int v, w; };
 std::vector<Edge> graph[LEN];
-int N, K, size[LEN], visited[LEN];
+int n, k, size[LEN], visited[LEN];
 std::map<int, int> min_dep;
 std::vector<Edge> cur_dep;
+void merge() {
+	for (const Edge& e : cur_dep) {
+		if (min_dep.find(e.v) == min_dep.end()) min_dep[e.v] = e.w;
+		else min_dep[e.v] = std::min(min_dep[e.v], e.w);
+	}
+	cur_dep.clear();
+}
 
 int get_size(int u, int p = -1) {
 	size[u] = 1;
@@ -28,11 +37,11 @@ int get_cent(int u, int p, int s) {
 }
 
 int get_path(int u, int p, int depth, int length) {
-	if (length == depth) return depth;
-	if (length > depth) return INF;
+	if (length == k) return depth;
+	if (length > k) return INF;
 	int result = INF;
-	if (min_dep.find(K - length) != min_dep.end())
-		result = std::min(result, min_dep[K - length] + depth);
+	if (min_dep.find(k - length) != min_dep.end())
+		result = std::min(result, min_dep[k - length] + depth);
 	cur_dep.push_back({ length, depth });
 	for (const Edge& e : graph[u]) {
 		if (e.v != p && !visited[e.v])
@@ -42,11 +51,40 @@ int get_path(int u, int p, int depth, int length) {
 }
 
 int f(int u) {
+	min_dep.clear();
 	int result = INF;
 	int cent = get_cent(u, -1, get_size(u));
-
+	// std::cout << "centroid: " << cent << '\n';
+	visited[cent] = true;
+	for (const Edge& e : graph[cent]) {
+		if (!visited[e.v])
+			result = std::min(result, get_path(e.v, u, 1, e.w));
+		merge();
+	}
+	for (const Edge& e : graph[cent]) {
+		if (!visited[e.v])
+			result = std::min(result, f(e.v));
+	}
+	return result;
 }
 
 int best_path(int N, int K, int H[][2], int L[]) {
+	n = N, k = K;
+	for (int i = 0; i < N - 1; ++i) {
+		int u = H[i][0], v = H[i][1], w = L[i];
+		graph[u].push_back({ v, w });
+		graph[v].push_back({ u, w });
+	}
+	int result = f(0);
+	return result != INF ? result : -1;
+}
 
+int N, K, H[LEN][2], L[LEN];
+int main() {
+	freopen("input.txt", "r", stdin);
+	std::cin >> N >> K;
+	for (int i = 0; i < N - 1; ++i) {
+		std::cin >> H[i][0] >> H[i][1] >> L[i];
+	}
+	std::cout << best_path(N, K, H, L);
 }

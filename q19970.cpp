c7786b75e823ac7bfb39cda0ffbf847d4ca3c93d
Jay <wnwoghd22@gmail.com>
2023-03-05 00:45:45 +0900

    update
    동생 절반, ChatGPT 절반 힘을 합쳐 풀었다.

--- q19970.cpp
+++ q19970.cpp
@@ -11,7 +11,7 @@ struct Vector {
 			x * r.y - y * r.x
 		};
 	}
-	
+	int magnitude() { return x * x + y * y + z * z; }
 } pos[8] = {
 	{ 0, 0, 0 },
 	{ 0, 0, 1 },
@@ -28,38 +28,50 @@ int line1[2], line2[2];
 
 int main() {
 	freopen("input.txt", "r", stdin);
-	std::cin >> s;
-	for (int i = 0, j = -1; s[i]; ++i) {
-		if (s[i] == '1') line1[j] += 4;
-		else line1[++j] = s[i] - 'A';
-	}
-	std::cin >> s;
-	for (int i = 0, j = -1; s[i]; ++i) {
-		if (s[i] == '1') line2[j] += 4;
-		else line2[++j] = s[i] - 'A';
-	}
-	if (line1[0] == line2[0] || line1[1] == line2[0] || line1[1] == line2[0] || line1[1] == line2[1]) {
-		std::cout << "Yes";
-	}
-	else {
-		// get plane
-		Vector p1 = pos[line1[0]];
-		Vector p2 = pos[line1[1]];
-		Vector p3 = pos[line2[0]];
-		Vector p4 = pos[line2[1]];
-
-		Vector v1 = p2 - p1;
-		Vector v2 = p3 - p1;
-		Vector norm = v1 * v2;
-		int d = p1.x * norm.x + p1.y * norm.y + p1.z * norm.z;
-		std::cout << norm.x << ' ' << norm.y << ' ' << norm.z << ' ' << d << '\n';
-		int result = norm.x * p4.x + norm.y * p4.y + norm.z * p4.z + d;
-		if (result) {
-			// std::cout << "not on a plane.\n";
-			std::cout << "No";
+	int T;
+	std::cin >> T;
+	while (T--) {
+		std::cin >> s;
+		for (int i = 0, j = -1; s[i]; ++i) {
+			if (s[i] == '1') line1[j] += 4;
+			else line1[++j] = s[i] - 'A';
+		}
+		std::cin >> s;
+		for (int i = 0, j = -1; s[i]; ++i) {
+			if (s[i] == '1') line2[j] += 4;
+			else line2[++j] = s[i] - 'A';
+		}
+		if (line1[0] == line2[0] || line1[1] == line2[0] || line1[1] == line2[0] || line1[1] == line2[1]) {
+			std::cout << "Yes\n";
 		}
 		else {
-			std::cout << "On the same plane.";
+			// get plane
+			Vector p1 = pos[line1[0]];
+			Vector p2 = pos[line1[1]];
+			Vector p3 = pos[line2[0]];
+			Vector p4 = pos[line2[1]];
+
+			Vector v1 = p2 - p1;
+			Vector v2 = p3 - p1;
+			Vector norm = v1 * v2;
+			std::cout << v1.x << ' ' << v1.y << ' ' << v1.z << '\n';
+			std::cout << v2.x << ' ' << v2.y << ' ' << v2.z << '\n';
+			std::cout << norm.x << ' ' << norm.y << ' ' << norm.z << '\n';
+
+			int d = p1.x * norm.x + p1.y * norm.y + p1.z * norm.z;
+			std::cout << norm.x << ' ' << norm.y << ' ' << norm.z << ' ' << -d << '\n';
+			int result = norm.x * p4.x + norm.y * p4.y + norm.z * p4.z - d;
+			if (result) {
+				std::cout << "not on a plane.\n";
+				std::cout << "No\n";
+			}
+			else {
+				std::cout << "On the same plane.\n";
+				v2 = p4 - p3;
+				Vector cross = v1 * v2;
+				if (cross.magnitude()) std::cout << "Yes\n";
+				else std::cout << "No\n";
+			}
 		}
 	}
 }

17dc420f7af84e654a1bce4dda74dbeb2439c65c
Jay <wnwoghd22@gmail.com>
2023-03-09 01:39:45 +0900

    Update swea16006.cpp

--- swea16006.cpp
+++ swea16006.cpp
@@ -11,16 +11,17 @@ int N;
 ll V[61];
 ll d[61];
 ll g[61] = { 2, };
-int c[61];
+int c[61], S[61];
 
-int log(int base, int n) {
-	int result = 0, k = 1;
+int log(ll base, int n) {
+	ll result = 0, k = 1;
 	while ((k *= base) <= n) ++result;
 	return result;
 }
 
 int solve() {
 	memset(c, 0, sizeof c);
+	memset(S, 0, sizeof S);
 	std::cin >> N;
 	std::cin >> V[0];
 	for (int i = 1; i < N; ++i) {
@@ -28,27 +29,24 @@ int solve() {
 		d[i - 1] = V[i] / V[i - 1];
 	}
 	std::sort(d, d + N - 1);
-	for (int i = 0; i < N - 1; ++i) std::cout << d[i] << ' ';
-	std::cout << '\n';
-	// classify
+
 	int pivot = 0;
 	for (int i = 0; i < N - 1; ++i) {
 		if (g[pivot] != d[i]) g[++pivot] = d[i];
 		++c[pivot];
 	}
-	for (int i = 0; i <= pivot; ++i) std::cout << g[i] << ' ';
-	std::cout << '\n';
-	for (int i = 0; i <= pivot; ++i) std::cout << c[i] << ' ';
-	std::cout << '\n';
+	S[0] = c[0];
+	for (int i = 1; i <= pivot; ++i) {
+		S[i] = S[i - 1] + c[i];
+	}
 
 	int max = 0;
-	for (int i = 0; i <= pivot; ++i) max = std::max(max, log(g[i], c[i]) + 1);
-	
+	for (int i = 0; i <= pivot; ++i) max = std::max(max, log(g[i], S[i]) + 1);
+
 	return max;
 }
 
 int main() {
-	freopen("input.txt", "r", stdin);
 	int t, T;
 	std::cin >> T;
 	for (t = 1; t <= T; ++t) std::cout << '#' << t << ' ' << solve() << '\n';

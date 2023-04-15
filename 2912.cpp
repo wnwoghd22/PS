19af0247c352f3cf4719df8b783ebe04c8b90126
Jay <wnwoghd22@gmail.com>
2023-04-15 12:19:11 +0900

    update
    트리와 쿼리 10 작성중

--- 2912.cpp
+++ 2912.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 #include <cmath>
 #include <algorithm>
+#include <set>
 
 const int LEN = 300'001;
 
-int N, C, M, sqrtN, A[LEN], answer[LEN], count[LEN], num[LEN] = { LEN, }, max = 0;
+int N, C, M, sqrtN, A[LEN], answer[LEN], count[LEN], max = 0;
+std::set<int> buckets[LEN];
 
 void add_count(int n) {
 	if (count[n] == max) ++max;
-	--num[count[n]++], ++num[count[n]];
+	buckets[count[n]++].erase(n);
+	buckets[count[n]].insert(n);
 }
 
 void pop_count(int n) {
-	if (count[n] == max && num[count[n]] == 1) --max;
-	--num[count[n]--], ++num[count[n]];
+	if (count[n] == max && buckets[count[n]].size() == 1) --max;
+	buckets[count[n]--].erase(n);
+	buckets[count[n]].insert(n);
 }
 
 struct Query {
@@ -25,6 +29,7 @@ int main() {
 	std::cin >> N >> C;
 	sqrtN = sqrt(N);
 	for (int i = 1; i <= N; ++i) std::cin >> A[i];
+	for (int i = 1; i <= C; ++i) buckets[0].insert(i);
 	std::cin >> M;
 	for (int i = 0; i < M; ++i) {
 		q[i].index = i;
@@ -33,13 +38,15 @@ int main() {
 	std::sort(q, q + M);
 	int s = q[0].s, e = q[0].e;
 	for (int i = s; i <= e; ++i) add_count(A[i]);
-	answer[q[0].index] = max > (e - s + 1) / 2;
+	answer[q[0].index] = max > (e - s + 1) / 2 ? *buckets[max].begin() : -1;
 	for (int i = 1; i < M; ++i) {
 		while (s < q[i].s) pop_count(A[s++]);
 		while (s > q[i].s) add_count(A[--s]);
 		while (e < q[i].e) add_count(A[++e]);
 		while (e > q[i].e) pop_count(A[e--]);
-		answer[q[i].index] = max > (e - s + 1) / 2;
+		answer[q[i].index] = max > (e - s + 1) / 2 ? *buckets[max].begin() : -1;
 	}
-	for (int i = 0; i < M; ++i) std::cout << answer[i] << '\n';
+	for (int i = 0; i < M; ++i)
+		if (~answer[i]) std::cout << "yes " << answer[i] << '\n';
+		else std::cout << "no\n";
 }
--- q13519.cpp
+++ q13519.cpp
@@ -19,6 +19,7 @@ struct Node {
 			std::max({ max, r.max, rm + r.lm })
 		};
 	}
+	Node operator~() const { return { rm, lm, sum, max, lazy, val }; }
 } seg_tree[LEN * 4];
 
 void propagate(int s, int e, int i) {
@@ -70,22 +71,71 @@ int ch_ord, chain[LEN], chain_size[LEN], chain_top[LEN];
 int dfs(int u, int p = 0) {
 	level[u] = level[p] + 1;
 	parent[u][0] = p;
-	
+	for (int i = 1, v = p; v; v = parent[u][i++])
+		parent[u][i] = parent[v][i - 1];
+	for (const int& v : graph[u]) {
+		if (v == p) continue;
+		int sub_size = dfs(v, u);
+		if (!heavy[u] || sub_size > size[heavy[u]]) heavy[u] = v;
+		size[u] += sub_size;
+	}
+	return ++size[u];
 }
 
 void dfs_euler(int u, int p = 0) {
-
+	order[u] = ++ord;
+	chain[u] = ch_ord;
+	if (!chain_size[chain[u]]++)
+		chain_top[chain[u]] = u;
+	
+	if (heavy[u]) dfs_euler(heavy[u], u);
+	for (const int& v : graph[u]) {
+		if (v == p || v == heavy[u]) continue;
+		++ch_ord;
+		dfs_euler(v, u);
+	}
 }
 
 int lca(int u, int v) {
-
+	if (level[u] ^ level[v]) {
+		if (level[u] > level[v]) std::swap(u, v);
+		for (int i = 19; i >= 0; --i)
+			if (level[parent[v][i]] >= level[u])
+				v = parent[v][i];
+	}
+	int l = u;
+	if (u ^ v) {
+		for (int i = 19; i >= 0; --i) {
+			if (parent[u][i] ^ parent[v][i]) {
+				u = parent[u][i];
+				v = parent[v][i];
+			}
+			l = parent[u][i];
+		}
+	}
+	return l;
 }
 
 ll query1(int u, int v) {
 	if (order[u] > order[v]) std::swap(u, v);
 	int l = lca(u, v);
 	Node total = { 0, 0, 0, 0 };
+	Node nu = { 0, 0, 0, 0 };
+	Node nv = { 0, 0, 0, 0 };
+	if (chain[u] ^ chain[v]) {
+		while (chain[u] ^ chain[l]) {
+			
+			update(order[chain_top[chain[u]]], order[u], pivot - level[chain_top[chain[u]]] + level[l], -1);
+			u = parent[chain_top[chain[u]]][0];
+		}
+		while (chain[v] ^ chain[l]) {
+			update(order[chain_top[chain[v]]], order[v], pivot + level[chain_top[chain[v]]] - level[l], 1);
+			v = parent[chain_top[chain[v]]][0];
+		}
+	}
 
+	if (level[u] <= level[v]) update(order[u], order[v], pivot, 1);
+	else update(order[v], order[u], pivot, -1);
 }
 
 ll query2(int u, int v) {

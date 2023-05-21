4db194b2e7a22b6d3f028b188c16c190b6f82925
Jay <wnwoghd22@gmail.com>
2023-05-21 10:15:03 +0900

    update
    생각을 너무 단순하게 했다.
    실수도 많았다.
    꽤 삽질을 해야 했다.

--- q24896.cpp
+++ q24896.cpp
@@ -7,7 +7,16 @@ std::vector<int> graph[LEN];
 int degree[LEN], checked[LEN];
 int N, result;
 
+void dfs(int u, int mask) {
+	checked[u] = mask;
+	for (const int& v : graph[u]) {
+		if (!checked[v] && degree[v] == 2)
+			dfs(v, mask);
+	}
+}
+
 int main() {
+	std::cin.tie(0)->sync_with_stdio(0);
 	std::cin >> N;
 	for (int i = 1, a, b; i < N; ++i) {
 		std::cin >> a >> b;
@@ -16,21 +25,22 @@ int main() {
 		degree[a]++;
 		degree[b]++;
 	}
-	for (int i = 1; i <= N; ++i) checked[i] = degree[i] == 1;
-	for (int u = 1; u <= N; ++u)
-		if (checked[u])
-			for (const int& v : graph[u])
-				--degree[v];
-	
-	for (int i = 1; i <= N; ++i) {
-		if (!checked[i] && degree[i] == 1)
-			checked[i] = 2;
+
+	for (int d = 1; d <= 2; ++d) {
+		for (int u = 1; u <= N; ++u)
+			if (!checked[u] && degree[u] == 1) {
+				checked[u] = d;
+				for (const int& v : graph[u])
+					if (!checked[v] && degree[v] == 2)
+						dfs(v, d);
+			}
+
+		for (int u = 1; u <= N; ++u)
+			if (checked[u] == d)
+				for (const int& v : graph[u])
+					--degree[v];
 	}
-	for (int u = 1; u <= N; ++u)
-		if (checked[u] == 2)
-			for (const int& v : graph[u])
-				--degree[v];
-	
+
 	for (int i = 1; i <= N; ++i)
 		if (!checked[i] && degree[i] > 2)
 			result = 1;

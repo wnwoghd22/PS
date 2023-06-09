3af10b112afd81ae0d8cbe59272c2af179808181
Jay <wnwoghd22@gmail.com>
2023-06-09 09:10:29 +0900

    update
    solution을 봤는데, 논리가 굉장히 우아하다.
    선행, 후행 프로세스의 수를 몰라도, 필요충분조건을 이용하면 critical project인지 빠르게 판단할 수 있다.

--- q12823.cpp
+++ q12823.cpp
@@ -5,14 +5,52 @@
 const int LEN = 100'001;
 
 std::vector<int> graph[LEN];
-bool visited[LEN];
 
 int incoming[LEN];
-int next[LEN];
+int ord, order[LEN];
+int level[LEN], cnt[LEN];
+int max, num, critical[LEN];
 
-int dfs(int u) {
-	int cnt = 0;
-	for (const int& v : graph[u]) {
-		// if (visited[v]) next[u] += 
+int N, M;
+
+void topological_sort() {
+	std::queue<int> q;
+
+	for (int u = 1; u <= N; ++u) {
+		if (!incoming[u])
+			q.push(u);
 	}
+
+	while (q.size()) {
+		int u = q.front(); q.pop();
+		++cnt[level[u]];
+		order[ord++] = u;
+		for (const int& v : graph[u]) {
+			if (!--incoming[v]) {
+				level[v] = level[u] + 1;
+				q.push(v);
+			}
+		}
+	}
+}
+
+int main() {
+	std::cin >> N >> M;
+	for (int i = 0, u, v; i < M; ++i) {
+		std::cin >> u >> v;
+		graph[u].push_back(v);
+		++incoming[v];
+	}
+	topological_sort();
+
+	for (int i = 0, m; i < N; ++i) {
+		int u = order[i];
+		if (cnt[level[u]] == 1 && max <= level[u]) critical[num++] = u;
+		m = 1e9;
+		for (const int& v : graph[u])
+			m = std::min(m, level[v]);
+		max = std::max(m, max);
+	}
+	std::cout << num << '\n';
+	for (int i = 0; i < num; ++i) std::cout << critical[i] << ' ';
 }

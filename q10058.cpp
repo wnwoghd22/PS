d75e461d12fdb892eac3a812158ee940076f26fc
Jay <wnwoghd22@gmail.com>
2023-06-13 08:59:56 +0900

    update
    최소 커버 찾는 방법을 구현.

--- q10058.cpp
+++ q10058.cpp
@@ -17,7 +17,6 @@ int cross(const Pos& p1, const Pos& p2, const Pos& p3) {
 	if (mul) mul /= std::abs(mul);
 	return mul;
 }
-bool a[LEN][LEN];
 std::vector<int> vert_u, vert_v;
 
 int u_len, v_len;
@@ -82,18 +81,29 @@ int hopcroft_karp() {
 	return result;
 }
 
+bool visited[LEN], result[LEN];
+void dfs_mask(int u) {
+	visited[u] = true;
+	for (const int& v : graph[u]) {
+		if (U[u] == v || visited[v]) continue;
+		visited[v] = true;
+		dfs_mask(V[v]);
+	}
+}
+
 int _u, _v, m;
 
 int check(int u, int v) {
-	int count = 2;
+	int count = 2, r = dist(pos[u], pos[v]);
 	vert_u.clear();
 	vert_v.clear();
 	for (std::vector<int>& v : graph) v.clear();
 	for (int i = 1; i <= N; ++i) {
-		if (a[u][i] && a[v][i]) {
+		if (i == u || i == v) continue;
+		if (dist(pos[u], pos[i]) <= r && dist(pos[v], pos[i]) <= r) {
 			++count;
 			int side = cross(pos[u], pos[v], pos[i]);
-			std::cout << u << ' ' << v << ' ' << i << ' ' << side << '\n';
+			// std::cout << u << ' ' << v << ' ' << i << ' ' << side << '\n';
 			if (side < 0) vert_u.push_back(i);
 			if (side > 0) vert_v.push_back(i);
 		}
@@ -102,14 +112,14 @@ int check(int u, int v) {
 	v_len = vert_v.size();
 	for (const int& i : vert_u)
 		for (const int& j : vert_v)
-			if (!a[i][j])
+			if (dist(pos[i], pos[j]) > r)
 				graph[i].push_back(j);
 
 	return count - hopcroft_karp();
 }
 
 int main() {
-	freopen("input.txt", "r", stdin);
+	// freopen("input.txt", "r", stdin);
 	// freopen("output.txt", "w", stdout);
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cin >> N >> d;
@@ -117,18 +127,8 @@ int main() {
 
 	for (int i = 1; i <= N; ++i) std::cin >> pos[i].x >> pos[i].y;
 	for (int i = 1; i < N; ++i) {
-		for (int j = i + 1; j <= N; ++j)
-			if (dist(pos[i], pos[j]) <= d2)
-				a[i][j] = a[j][i] = 1;
-	}
-	for (int i = 1; i <= N; ++i) {
-		for (int j = 1; j <= N; ++j)
-			std::cout << a[i][j];
-		std::cout << '\n';
-	}
-	for (int i = 1; i < N; ++i) {
 		for (int j = i + 1; j <= N; ++j) {
-			if (a[i][j]) {
+			if (dist(pos[i], pos[j]) <= d2) {
 				int c = check(i, j);
 				if (c > m) {
 					m = c;
@@ -140,8 +140,46 @@ int main() {
 	if (!m) std::cout << "1\n1";
 	else if (m) {
 		std::cout << m << '\n';
-		check(_u, _v);
-		std::cout << _u << ' ' << _v << ' ';
-		for (int i = 1; i <= N; ++i) std::cout << U[i] << ' ' << V[i] << '\n';
+		vert_u.clear();
+		vert_v.clear();
+		result[_u] = result[_v] = true;
+		int r = dist(pos[_u], pos[_v]);
+		for (std::vector<int>& v : graph) v.clear();
+		for (int i = 1; i <= N; ++i) {
+			if (i == _u || i == _v) continue;
+			if (dist(pos[_u], pos[i]) <= r && dist(pos[_v], pos[i]) <= r) {
+				result[i] = true;
+				int side = cross(pos[_u], pos[_v], pos[i]);
+				// std::cout << u << ' ' << v << ' ' << i << ' ' << side << '\n';
+				if (side < 0) vert_u.push_back(i);
+				if (side > 0) vert_v.push_back(i);
+			}
+		}
+		u_len = vert_u.size();
+		v_len = vert_v.size();
+		for (const int& i : vert_u)
+			for (const int& j : vert_v)
+				if (dist(pos[i], pos[j]) > r)
+					result[i] = result[j] = false,
+					graph[i].push_back(j);
+
+		hopcroft_karp();
+
+		for (const int& u : vert_u) {
+			if (!U[u])
+				dfs_mask(u);
+		}
+		for (const int& u : vert_u) {
+			if (visited[u])
+				result[u] = true;
+		}
+		for (const int& v : vert_v) {
+			if (!visited[v])
+				result[v] = true;
+		}
+
+		for (int i = 1; i <= N; ++i)
+			if (result[i])
+				std::cout << i << ' ';
 	}
 }

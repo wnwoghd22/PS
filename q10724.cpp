f4914181e81cf5648cca01d40f3161427b82106a
Jay <wnwoghd22@gmail.com>
2023-07-16 13:59:15 +0900

    update
    시간초과 해결.
    dfs가 문제였다면, 포인터는 빼고 이것만 해도 되지 않을까?

--- q10724.cpp
+++ q10724.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <vector>
 
 typedef long long ll;
 const int LEN = 300'001;
@@ -14,94 +15,86 @@ struct LinkCutTree {
 		int s; // size
 		bool f; // flip
 		int v, i, max, max_i;
+		void update() {
+			s = 1;
+			max = v;
+			max_i = i;
+			if (l) {
+				s += l->s;
+				if (l->max > max) {
+					max = l->max;
+					max_i = l->max_i;
+				}
+			}
+			if (r) {
+				s += r->s;
+				if (r->max > max) {
+					max = r->max;
+					max_i = r->max_i;
+				}
+			}
+		}
+		bool is_root() { return !p || (p->l != this && p->r != this); }
+		bool is_left() { return p->l == this; }
+		void rotate() {
+			if (is_left()) r && (r->p = p), p->l = r, r = p;
+			else l && (l->p = p), p->r = l, l = p;
+			
+			if (!p->is_root()) (p->is_left() ? p->p->l : p->p->r) = this;
+			Node* y = p; p = y->p; y->p = this;
+			y->update(); update();
+		}
+		void push() {
+			if (f) {
+				if (l) l->f ^= 1;
+				if (r) r->f ^= 1;
+				std::swap(l, r);
+				f = 0;
+			}
+		}
 	} t[LEN];
-	void update(Node* x) {
-		x->s = 1;
-		x->max = x->v;
-		x->max_i = x->i;
-		if (x->l) {
-			x->s += x->l->s;
-			if (x->l->max > x->max) {
-				x->max = x->l->max;
-				x->max_i = x->l->max_i;
-			}
-		}
-		if (x->r) {
-			x->s += x->r->s;
-			if (x->r->max > x->max) {
-				x->max = x->r->max;
-				x->max_i = x->r->max_i;
-			}
-		}
-	}
-	bool is_root(Node* x) { return !x->p || (x->p->l != x && x->p->r != x); }
-	bool is_left(Node* x) { return x->p->l == x; }
-	void rotate(Node* x) {
-		Node* p = x->p;
-		if (is_left(x)) {
-			if (x->r) x->r->p = p;
-			p->l = x->r; x->r = p;
-		}
-		else {
-			if (x->l) x->l->p = p;
-			p->r = x->l; x->l = p;
-		}
-		if (!is_root(p)) (is_left(p) ? p->p->l : p->p->r) = x;
-		x->p = p->p; p->p = x;
-		update(p); update(x);
-	}
-	void push(Node* x) {
-		if (x->f) {
-			if (x->l) x->l->f ^= 1;
-			if (x->r) x->r->f ^= 1;
-			std::swap(x->l, x->r);
-			x->f = 0;
-		}
-	}
 	void splay(Node* x) {
-		for (Node* p; !is_root(x); rotate(x)) {
+		for (Node* p; !x->is_root(); x->rotate()) {
 			p = x->p;
-			if (!is_root(p)) push(p->p);
-			push(p); push(x);
-			if (is_root(p)) continue;	// zig
-			if (is_left(x) == is_left(p)) rotate(p);	// zig-zig
-			else rotate(x);	// zig-zag
+			if (!p->is_root()) p->p->push(); p->push(); x->push();
+			if (p->is_root()) continue;	// zig
+			if (x->is_left() == p->is_left()) p->rotate();	// zig-zig
+			else x->rotate();	// zig-zag
 		}
-		push(x);
+		x->push();
 	}
 	void access(Node* x) {
 		splay(x); x->r = 0;
-		for (Node* p; x->p; splay(x)) {
-			p = x->p;
-			splay(p); p->r = x;
-		}
+		for (; x->p; splay(x))
+			splay(x->p), x->p->r = x;
 	}
 	void make_root(Node* x) {
 		access(x);
 		x->f ^= 1;
 	}
 	void link(Node* x, Node* p) {
-		make_root(x); push(x);
+		access(x);
 		access(p);
 		p->p = x; x->l = p;
+		x->update();
 	}
 	void cut(Node* x) {
 		access(x);
-		// if (!x->l) return;
-		x->l->p = 0;
-		x->l = 0;
+		if (!x->l) return;
+		x->l = x->l->p = 0;
+		x->update();
 	}
 	Node* get_root(Node* x) {
 		access(x);
-		while (x->l) x = x->l, push(x);
+		while (x->l) x = x->l, x->push();
 		splay(x);
 		return x;
 	}
 	Node* get_parent(Node* x) {
 		access(x);
 		if (!x->l) return 0;
-		x = x->l; push(x);
-		while (x->r) x = x->r, push(x);
+		x = x->l; x->push();
+		while (x->r) x = x->r, x->push();
 		splay(x);
 		return x;
 	}
@@ -119,23 +112,15 @@ struct LinkCutTree {
 	}
 	Result query(Node* x, Node* y) {
 		Node* l = get_lca(x, y);
-		Result result = { l->max, l->max_i };
+		Result result = { l->v, l->i };
 
 		access(x); splay(l);
-		if (l->r) {
-			if (l->r->max > result.max) {
-				result.max = l->r->max;
-				result.max_i = l->r->max_i;
-			}
-		}
+		if (l->r && l->r->max > result.max)
+			result = { l->r->max, l->r->max_i };
 
 		access(y); splay(l);
-		if (l->r) {
-			if (l->r->max > result.max) {
-				result.max = l->r->max;
-				result.max_i = l->r->max_i;
-			}
-		}
+		if (l->r && l->r->max > result.max)
+			result = { l->r->max, l->r->max_i };
 
 		return result;
 	}
@@ -143,30 +128,43 @@ struct LinkCutTree {
 	void update(Node* x, int d) {
 		splay(x);
 		x->v = d;
-		update(x);
+		x->update();
 	}
 
+	void make_root(int u) { make_root(&t[u]); }
 	void link(int u, int v) { link(&t[u], &t[v]); }
 	void cut(int u, int v) { cut(&t[u], &t[v]); }
 	Result query(int u, int v) { return query(&t[u], &t[v]); }
 } lct;
 
 struct Edge { int u, v; } edges[LEN];
+std::vector<Edge> graph[100'001];
+void dfs(int u, int p = 0) {
+	for (const Edge& e : graph[u]) {
+		if (e.u == p) continue;
+		dfs(e.u, u);
+		lct.t[N + e.u].i = lct.t[N + e.u].max_i = N + e.u;
+		lct.t[N + e.u].v = lct.t[N + e.u].max = e.v;
+		lct.link(e.u, N + e.u);
+		lct.link(N + e.u, u);
+		edges[N + e.u] = { u, e.u };
+	}
+}
 
 ll solve() {
 	ll answer = 0, mst = 0;
+	for (std::vector<Edge>& v : graph) v.clear();
 	memset(lct.t, 0, sizeof lct.t);
 
 	std::cin >> N >> M;
 	for (int i = 2, u, c; i <= N; ++i) {
 		std::cin >> u >> c; ++u;
-		lct.t[N + i].i = lct.t[N + i].max_i = N + i;
-		lct.t[N + i].v = lct.t[N + i].max = c;
-		lct.link(i, N + i);
-		lct.link(N + i, u);
-		edges[N + i] = { i, u };
+		graph[i].push_back({ u, c });
+		graph[u].push_back({ i, c });
 		mst += c;
 	}
+
+	dfs(1);
 
 	for (int j = 1, u, v, c; j <= M; ++j) {
 		std::cin >> u >> v >> c; ++u, ++v;
@@ -183,6 +181,7 @@ ll solve() {
 			mst += c - cur.max;
 			lct.cut(edges[cur.max_i].u, cur.max_i);
 			lct.cut(edges[cur.max_i].v, cur.max_i);
+			lct.make_root(u);
 			lct.link(u, N * 2 + j);
 			lct.link(N * 2 + j, v);
 		}

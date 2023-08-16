c51e66b4058a7022754dfb749a0b59b1679d63de
Jay <wnwoghd22@gmail.com>
2023-08-17 00:29:59 +0900

    Update q13512.cpp

--- q13512.cpp
+++ q13512.cpp
@@ -25,9 +25,27 @@ int get_min(int l, int r, int s = 1, int e = N, int i = 1) {
 std::vector<int> graph[LEN];
 int level[LEN], size[LEN], parent[LEN];
 int ord, order[LEN], heavy[LEN];
-int ch_ord, chain[LEN];
+int ch_ord, chain[LEN], ch_top[LEN];
 
+int dfs(int u, int p = 0) {
+	level[u] = level[p] + 1;
+	size[u] = 1;
+	
+	for (const int& v : graph[u]) {
+		if (v == p) continue;
+		int sub = dfs(v, u);
+		if (!heavy[u] || sub > size[heavy[u]]) 
+			heavy[u] = v;
+		size[u] += sub;
+	}
+	return size[u];
+}
 
+void dfs_euler(int u, int p = 0) {
+	order[u] = ++ord;
+	chain[u] = ch_ord;
+
+}
 
 int main() {
 

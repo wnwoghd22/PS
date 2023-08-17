b879fb5c9f5de69c83c7a3eb782382a9abace9a6
Jay <wnwoghd22@gmail.com>
2023-08-18 01:47:43 +0900

    update

---
+++ q17069.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+typedef long long ll;
+
+int N;
+ll dp[33][33][3];
+int map[33][33];
+
+ll F() {
+	dp[1][2][0] = 1;
+	for (int i = 1; i <= N; ++i) {
+		for (int j = 1; j <= N; ++j) {
+			if (map[i][j]) continue;
+
+			dp[i][j][0] += dp[i][j - 1][0] + dp[i][j - 1][2];
+			dp[i][j][1] += dp[i - 1][j][1] + dp[i - 1][j][2];
+
+			if (map[i - 1][j] || map[i][j - 1]) continue;
+
+			dp[i][j][2] += dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
+		}
+	}
+	return dp[N][N][0] + dp[N][N][1] + dp[N][N][2];
+}
+
+int main() {
+	std::cin >> N;
+	for (int i = 1; i <= N; ++i)
+		for (int j = 1; j <= N; ++j)
+			std::cin >> map[i][j];
+
+	std::cout << F();
+}

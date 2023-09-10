b39cf1ba19b45af6c22a7863d1057be062703733
Jay <wnwoghd22@gmail.com>
2023-09-10 12:34:03 +0900

    update

--- q25430.cpp
+++ q25430.cpp
@@ -1,57 +1,43 @@
 #include <iostream>
-#include <vector>
-#include <queue>
-#include <map>
+#include <algorithm>
 
 typedef long long ll;
-const int LEN = 50001;
+const ll INF = 1e13;
+const int NLEN = 50'001;
+const int MLEN = 100'000;
 
-struct I {
-	ll d;
-	int w;
-	int u;
-	bool operator<(const I& r) const {
-		return d == r.d ? w > r.w : d > r.d;
-	}
-};
-struct Ed { int v, w; };
+struct Edge {
+	int u, v, w;
+	bool operator<(const Edge& r) const { return w < r.w; }
+} edges[MLEN];
 
-std::priority_queue<I> pq;
-std::vector<Ed> graph[LEN];
-std::map<int, ll> dist[LEN];
-
-int N, M, S, E;
+int N, M,S, E;
+ll dp[2][NLEN];
 
 int main() {
 	std::cin >> N >> M;
-	for (int i = 0, u, v, w; i < M; ++i) {
-		std::cin >> u >> v >> w;
-		graph[u].push_back({ v, w });
-		graph[v].push_back({ u, w });
-	}
+	for (int i = 0; i < M; ++i) std::cin >> edges[i].u >> edges[i].v >> edges[i].w;
 	std::cin >> S >> E;
 
-	dist[S][0] = 0;
-	pq.push({ 0, 0, S });
-	while (pq.size()) {
-		int u = pq.top().u, w = pq.top().w;
-		ll d = pq.top().d; pq.pop();
-		if (dist[u][w] < d) continue;
+	for (int i = 1; i <= N; ++i) dp[0][i] = dp[1][i] = INF;
+	dp[0][S] = dp[1][S] = 0;
 
-		for (const Ed& e : graph[u]) {
-			if (e.w <= w) continue;
-			if (dist[e.v].find(e.w) == dist[e.v].end() || dist[e.v][e.w] < d + e.w) {
-				dist[e.v][e.w] = d + e.w;
-				pq.push({ d + e.w, e.w, e.v });
-			}
+	std::sort(edges, edges + M);
+
+	for (int u, v, w, s, m, e = 0; e < M; ++e) {
+		for (s = e; e < M; ++e) {
+			u = edges[e].u, v = edges[e].v, w = edges[e].w;
+			dp[1][u] = std::min(dp[1][u], dp[0][v] + w);
+			dp[1][v] = std::min(dp[1][v], dp[0][u] + w);
+			if (e == M - 1 || edges[e + 1].w != edges[e].w) break;
+		}
+		for (m = s; m <= e; ++m) {
+			u = edges[m].u, v = edges[m].v, w = edges[m].w;
+			dp[0][u] = std::min(dp[0][u], dp[1][u]); dp[1][u] = INF;
+			dp[0][v] = std::min(dp[0][v], dp[1][v]); dp[1][v] = INF;
 		}
 	}
-	if (dist[E].empty()) std::cout << "DIGESTA";
-	else {
-		ll min = 1e17;
-		for (const auto&[k, v] : dist[E]) {
-			min = std::min(min, v);
-		}
-		std::cout << min;
-	}
+
+	if (dp[0][E] == INF) std::cout << "DIGESTA";
+	else std::cout << dp[0][E];
 }

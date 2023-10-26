ae4ad47740e217821784cccb4ea22bfddd36a2f0
Jay <wnwoghd22@gmail.com>
2023-10-27 01:13:33 +0900

    update

--- q2419.cpp
+++ q2419.cpp
@@ -2,33 +2,31 @@
 #include <cstring>
 #include <algorithm>
 
-typedef long long ll;
-const ll INF = 1e17;
+const int INF = 1e9;
 const int LEN = 301;
 const int L = 0;
 const int R = 1;
 
 int N, M, A, a;
-ll S[LEN], dp[LEN][LEN][2];
+int S[LEN], dp[LEN][LEN][2];
 
-ll f(int s, int e, int r) {
-	if (s == 1 && e == N) return 0;
-	ll& ref = dp[s][e][r];
+int f(int s, int e, int r, int i) {
+	if (!i) return 0;
+	int& ref = dp[s][e][r];
 	if (~ref) return ref;
-	ref = 0;
-	// if (e < N) ref = std::max(ref, f(s, e + 1, R) + (N - e + s - 1) * (S[e + 1] - S[r ? e : s]));
-	// if (s > 1) ref = std::max(ref, f(s - 1, e, L) + (N - e + s - 1) * (S[r ? e : s] - S[s - 1]));
+	ref = INF;
+	if (e < N - 1) ref = std::min(ref, f(s, e + 1, R, i - 1) + i * (S[e + 1] - S[r ? e : s]));
+	if (s > 0) ref = std::min(ref, f(s - 1, e, L, i - 1) + i * (S[r ? e : s] - S[s - 1]));
 	return ref;
 }
 
 int main() {
-	memset(dp, -1, sizeof dp);
 	std::cin >> N >> M;
 	for (int i = 0; i < N; ++i) {
 		std::cin >> S[i];
 		if (!S[i]) A = 1;
 	}
-	if (A) ++N;
+	if (!A) ++N;
 	std::sort(S, S + N); 
 	for (int i = 0; i < N; ++i) {
 		if (!S[i]) {
@@ -36,6 +34,10 @@ int main() {
 			break;
 		}
 	}
-
-	std::cout << std::max(f(a, a, L), f(a, a, R)) + M * A << '\n';
+	int ans = 0;
+	for (int i = 0; i < N; ++i) {
+		memset(dp, -1, sizeof dp);
+		ans = std::max(ans, M * i - std::min(f(a, a, L, i), f(a, a, R, i)));
+	}
+	std::cout << ans + M * A << '\n';
 }

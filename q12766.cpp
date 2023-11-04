ca431bdcf7cfa61d46e2419080be79692d871a4f
Jay <wnwoghd22@gmail.com>
2023-11-04 16:21:53 +0900

    update

--- q12766.cpp
+++ q12766.cpp
@@ -8,7 +8,20 @@ const int LEN = 5001;
 const ll INF = 1e17;
 
 int N, b, s, r;
-ll dist[2][LEN], A[LEN], S[LEN];
+ll dist[2][LEN], A[LEN], S[LEN], dp[LEN][LEN];
+
+ll c(int g, ll i, ll j) { return dp[g - 1][i - 1] + (j - i) * (S[j] - S[i - 1]); }
+void f(int g, int s, int e, int l, int r) {
+	if (s > e) return;
+	int m = s + e >> 1;
+	int opt = l;
+	for (int i = opt; i <= std::min(m, r); ++i)
+		if (c(g, i, m) < c(g, opt, m)) opt = i;
+	dp[g][m] = c(g, opt, m);
+
+	f(g, s, m - 1, l, opt);
+	f(g, m + 1, e, opt, r);
+}
 
 struct Edge { int v, l; };
 std::vector<Edge> g[2][LEN];
@@ -45,4 +58,7 @@ int main() {
 	for (int i = 1; i <= b; ++i) {
 		std::cout << A[i] << ' ' << S[i] << '\n';
 	}
+	for (int i = 1; i <= b; ++i) dp[0][i] = S[i] * (i - 1);
+	for (int i = 1; i < s; ++i) f(i, 1, b, 1, b);
+	std::cout << dp[s - 1][b];
 }

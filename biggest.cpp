e2bc3760f72825e61d20386223a34f40c2b053bb
Jay <wnwoghd22@gmail.com>
2023-12-30 17:06:55 +0900

    update

--- biggest.cpp
+++ biggest.cpp
@@ -8,63 +8,45 @@ static int _size[100'001];
 
 struct Comp {
 	bool operator()(int a, int b) {
-		if (children[a].size() == children[b].size()) {
-			if (height[a] == height[b]) return _size[a] < _size[b];
-			return height[a] > height[b];
-		}
 		return children[a].size() > children[b].size();
 	}
 };
 
 static void merge(int a, int b) {
-	height[a] = std::max(height[a], height[b] + 1);
-	_size[a] += _size[b];
 	children[a].push_back(b);
 }
 
-static int init(int l, int r) {
-	if (l == r) return l;
+static int merge_two(int a, int b) {
+	int d = compare(a, b);
+	if (d == a) merge(a, b);
+	else merge(b, a);
+	return d;
+}
 
-	if (l + 1 == r) {
-		if (compare(l, r) == l) { 
-			merge(l, r); return l; 
-		}
-		else {
-			merge(r, l); return r;
-		}
-	}
-	int lmax = init(l, (l + r) / 2);
-	int rmax = init((l + r) / 2 + 1, r);
-
-	if (compare(lmax, rmax) == lmax) {
-		merge(lmax, rmax); return lmax;
-	}
-	else { 
-		merge(rmax, lmax); return rmax;
-	}
+static int merge_three(int a, int b, int c) {
+	int d = compare(a, b);
+	if (d == a) merge(a, b);
+	else merge(b, a);
+	int e = compare(c, d);
+	if (e == c) merge(c, d);
+	else merge(d, c);
+	return e;
 }
 
 static std::vector<int> biggest(int N, int K) {
 	std::vector<int> result;
 	std::priority_queue<int, std::vector<int>, Comp> pq;
 
-	// for (int i = 1; i <= N; ++i) pq.push(i);
-	for (int i = 1; i <= N; ++i) _size[i] = 1;
-	pq.push(init(1, N));
+	for (int i = 1; i <= N - 2; i += 3)
+		pq.push(merge_three(i, i + 1, i + 2));
+	if (N % 3 == 1) pq.push(N);
+	if (N % 3 == 2) pq.push(merge_two(N, N - 1));
 
 	while (result.size() != K) {
 		while (pq.size() != 1) {
 			int a = pq.top(); pq.pop();
 			int b = pq.top(); pq.pop();
-
-			if (compare(a, b) == a) {
-				merge(a, b);
-				pq.push(a);
-			}
-			else {
-				merge(b, a);
-				pq.push(b);
-			}
+			pq.push(merge_two(a, b));
 		}
 		int max = pq.top(); pq.pop();
 		result.push_back(max);

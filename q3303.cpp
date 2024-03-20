d8d0e3040d1459ff4fa3707dc6e009a03ea3d3dc
Jay <wnwoghd22@gmail.com>
2024-03-20 16:46:33 +0900

    update

--- q3303.cpp
+++ q3303.cpp
@@ -1,52 +1,50 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
+#include <vector>
 
+typedef long long ll;
 const int LEN = 200'001;
-int N, cnt[LEN], ord[LEN], SA[LEN], g[LEN], tg[LEN];
-int lcp[LEN], rank[LEN];
-char S[LEN];
 
-inline int min(int x, int y) { return x < y ? x : y; }
-inline bool compare(int x, int y, int t) { return g[x] == g[y] ? g[x + t] < g[y + t] : g[x] < g[y]; }
-void manber_myers(const int n, const char* s) {
-	for (int i = 0; i < n; ++i) SA[i] = i, g[i] = s[i];
-	for (int t = 1; t < n; t <<= 1) {
-		// radix sort by rank(i + 2^t)
-		memset(cnt, 0, sizeof cnt);
-		for (int i = 0; i < n; ++i) ++cnt[g[min(i + t, n)]];
-		for (int i = 1; i < LEN; ++i) cnt[i] += cnt[i - 1];
-		for (int i = n - 1; i >= 0; --i) ord[--cnt[g[min(i + t, n)]]] = i;
+struct Pos {
+	ll x, y;
+	ll s() const { return x * x + y * y; }
+	bool operator<(const Pos& r) const { return y * r.x < r.y * x; }
+	bool operator==(const Pos& r) const { return y * r.x == r.y * x; }
+	friend std::istream& operator>>(std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
+} O = { 0, 0 }, p[LEN];
+ll cross(const Pos& p1, const Pos& p2, const Pos& p3) { return (p2.x - p1.x) * (p3.y - p2.y) - (p2.y - p1.y) * (p3.x - p2.x); }
 
-		// radix sort by rank(i)
-		memset(cnt, 0, sizeof cnt);
-		for (int i = 0; i < n; ++i) ++cnt[g[i]];
-		for (int i = 1; i < LEN; ++i) cnt[i] += cnt[i - 1];
-		for (int i = n - 1; i >= 0; --i) SA[--cnt[g[ord[i]]]] = ord[i];
-
-		tg[SA[0]] = 1;
-		for (int i = 1; i < n; ++i)
-			tg[SA[i]] = tg[SA[i - 1]] + compare(SA[i - 1], SA[i], t);
-		for (int i = 0; i < n; ++i) g[i] = tg[i];
-	}
-}
-
-void get_lcp(const int n, const char* s) {
-	for (int i = 0; i < n; ++i) rank[SA[i]] = i;
-	int len = 0;
-	for (int i = 0, j, k; i < n; ++i) {
-		if (k = rank[i]) {
-			j = SA[k - 1];
-			while (s[i + len] == s[j + len]) ++len;
-			lcp[k] = len;
-			if (len) --len;
-		}
-	}
-}
+int N, L, R;
 
 int main() {
-	std::cin >> N >> S;
-	manber_myers(N, S);
-	get_lcp(N, S);
-	std::cout << *std::max_element(lcp + 1, lcp + N);
+	std::cin.tie(0)->sync_with_stdio(0);
+	std::cin >> N >> p[0];
+	for (int i = 0; i < N; ++i) {
+		std::cin >> p[i];
+		if (p[L] < p[i] || p[i] == p[L] && p[i].s() < p[L].s()) L = i;
+		if (p[i] < p[R] || p[i] == p[R] && p[i].s() < p[R].s()) R = i;
+	}
+
+	std::vector<int> stack; // monotonic, ccw order
+	stack.push_back(R);
+
+	for (int i = (R + 1) % N, j = R, visible = 1; i != L; ++i %= N, ++j %= N) {
+		// j -> i
+		ll dir = cross(O, p[j], p[i]);
+
+		if (visible) {
+
+		}
+		else {
+			if (p[stack.back()] < p[i]) {
+				// ???
+			}
+		}
+
+
+	}
+
+	stack.push_back(L);
+	std::sort(stack.begin(), stack.end());
+	for (const int& i : stack) std::cout << i << ' ';
 }

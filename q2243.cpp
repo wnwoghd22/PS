e6ede59d103497f719172c88d9e2edcf4169aec7
Jay <wnwoghd22@gmail.com>
2024-03-25 15:44:41 +0900

    update

--- q2243.cpp
+++ q2243.cpp
@@ -3,35 +3,20 @@
 const int MAX = 1'000'000;
 int N, A, B, C, seg_tree[MAX * 4];
 
-int get_sum(int left, int right, int index = 1, int start = 1, int end = MAX) {
-	if (left > end || right < start) return 0;
-	if (left <= start && end <= right) return seg_tree[index];
-	int mid = (start + end) / 2;
-	return get_sum(left, right, index * 2, start, mid) + get_sum(left, right, index * 2 + 1, mid + 1, end);
+void update(int x, int d, int s = 1, int e = MAX, int i = 1) {
+	if (x < s || e < x) return;
+	seg_tree[i] += d;
+	if (s == e) return;
+	int m = s + e >> 1;
+	update(x, d, s, m, i << 1);
+	update(x, d, m + 1, e, i << 1 | 1);
 }
 
-void update(int n, int diff, int index = 1, int start = 1, int end = MAX) {
-	if (n > end || n < start) return;
-	seg_tree[index] += diff;
-	if (start == end) return;
-	int mid = (start + end) / 2;
-	update(n, diff, index * 2, start, mid);
-	update(n, diff, index * 2 + 1, mid + 1, end);
-}
-
-int binary_search(const int n) {
-	int l = 1, r = MAX, mid, count, result = MAX;
-	while (l <= r) {
-		mid = (l + r) / 2;
-		count = get_sum(1, mid);
-		if (count >= n) {
-			result = std::min(mid, result);
-			r = mid - 1;
-		}
-		else l = mid + 1;
-	}
-	update(result, -1);
-	return result;
+int get_bound(int k, int s = 1, int e = MAX, int i = 1) {
+	if (s == e) return s;
+	int m = s + e >> 1;
+	if (seg_tree[i << 1] >= k) return get_bound(k, s, m, i << 1);
+	return get_bound(k - seg_tree[i << 1], m + 1, e, i << 1 | 1);
 }
 
 int main() {
@@ -39,6 +24,10 @@ int main() {
 	while (N--) {
 		std::cin >> A;
 		if (A - 1) std::cin >> B >> C, update(B, C);
-		else std::cin >> B, std::cout << binary_search(B) << '\n';
+		else {
+			std::cin >> B;
+			std::cout << (A = get_bound(B)) << '\n';
+			update(A, -1);
+		}
 	}
 }

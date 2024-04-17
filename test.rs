91c791854ee1c4fffaa945c9f40cfed314bcfe47
Jay <wnwoghd22@gmail.com>
2024-04-17 13:56:13 +0900

    Update test.rs

--- test.rs
+++ test.rs
@@ -1,49 +1,17 @@
-const DX: [i32; 4] = [1, -1, 0, 0];
-const DY: [i32; 4] = [1, -1, 0, 0];
-const DELTA: [(i32, i32); 4] = [(1, 0), (-1, 0), (0, 1), (0, -1)];
+use std::fs::File;
+use std::error::Error;
+use std::io::{self, Read, ErrorKind};
 
-#[derive(Debug)]
-struct Pos {
-    x: i64,
-    y: i64,
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut username = String::new();
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+    Ok(username)
 }
 
-impl Pos {
-    fn magnitude(&self) -> i64 { self.x * self.x + self.y * self.y }
-}
+fn main() -> Result<(), Box<dyn Error>> {
+    let greeting_file_result = File::open("hello.txt")?;
 
-enum IpAddrKind {
-    v4,
-    v6,
-}
+    let username = read_username_from_file().unwrap();
 
-fn take_ownership(some_str: String) {
-    println!("{}", some_str);
-}
-
-fn just_get_ref(some_str: &String) {
-    println!("{}", some_str);
-}
-
-
-fn main() {
-    let some_str = String::from("hello world!");
-    // take_ownership(some_str);
-    just_get_ref(&some_str);
-    println!("{}", some_str);
-
-    let arr: [i32; 5] = [1, 2, 3, 4, 5];
-   
-    let zero = Pos {
-        x: 0,
-        y: 0,
-    };
-
-    println!("{:?}", zero);
-
-    let p1 = Pos {
-        x: 3,
-        y: 4,
-    };
-    println!("{}", p1.magnitude());
+    Ok(())
 }

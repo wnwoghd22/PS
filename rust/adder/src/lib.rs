c3f4fc8187ba35299a6a156dd6f7581839c461a3
Jay <wnwoghd22@gmail.com>
2024-04-18 16:14:39 +0900

    update

--- rust/adder/src/lib.rs
+++ rust/adder/src/lib.rs
@@ -1,40 +1,9 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn add_two(a: i32) -> i32 {
+    internal_adder(a, 2)
 }
 
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
-impl Rectangle {
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
-}
-
-fn add_two(x: i32) -> i32 { x + 2 }
-
-pub fn greeting(name: &str) -> String {
-    format!("Hello {}!", name)
-}
-
-pub struct Guess {
-    value: i32,
-}
-
-impl Guess {
-    pub fn new(value: i32) -> Guess {
-        if value < 1 || value > 1_000_000_007 {
-            panic!("The secret number will be between 1 and 1,000,000,007, get {}.", value);
-        }
-        Guess { value }
-    }
-
-    pub fn value(&self) -> i32 {
-        self.value
-    }
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
 }
 
 #[cfg(test)]
@@ -42,64 +11,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn fuck() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-
-    #[test]
-    fn larger_can_hold_smaller() {
-        let larger = Rectangle {
-            width: 8,
-            height: 7,
-        };
-        let smaller = Rectangle {
-            width: 5,
-            height: 4,
-        };
-        assert!(larger.can_hold(&smaller));
-    }
-
-    #[test]
-    fn smaller_cannot_hold_larger() {
-        let larger = Rectangle {
-            width: 8,
-            height: 7,
-        };
-        let smaller = Rectangle {
-            width: 5,
-            height: 4,
-        };
-        assert!(!smaller.can_hold(&larger));
-    }
-
-    #[test]
-    fn test_add_two() {
-        assert_eq!(4, add_two(2));
-    }
-
-    #[test]
-    fn greeting_contains_name() {
-        let result = greeting("Carol");
-        assert!(
-            result.contains("Carol"),
-            "Greeting did not contain name, value was`{}`",
-            result
-        );
-    }
-
-    #[test]
-    #[should_panic]
-    fn greater_than_max() {
-        Guess::new(2_000_000_000);
-    }
-
-    #[test]
-    fn it_works() -> Result<(), String> {
-        if 2 + 2 == 4 {
-            Ok(())
-        } else {
-            Err(String::from("two plus two does not equal four"))
-        }
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2));
     }
 }

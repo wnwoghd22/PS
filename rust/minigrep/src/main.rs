7c8186a7ebca71646231054adc24c64be6a72bc5
Jay <wnwoghd22@gmail.com>
2024-04-20 13:10:54 +0900

    update

--- rust/minigrep/src/main.rs
+++ rust/minigrep/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::process;
 
-use jay22_minigrep::Config;
+use jay202_minigrep::Config;
 
 fn main() {
     let config = Config::build(env::args()).unwrap_or_else(|err| {
@@ -9,7 +9,7 @@ fn main() {
         process::exit(0);
     });
 
-    if let Err(e) = jay22_minigrep::run(config) {
+    if let Err(e) = jay202_minigrep::run(config) {
         eprintln!("Application error: {e}");
         process::exit(1);
     }

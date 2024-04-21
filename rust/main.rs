858a89356124cdc7e2fe1808f54194cf9ea8b899
Jay <wnwoghd22@gmail.com>
2024-04-21 15:12:12 +0900

    Update main.rs

--- rust/main.rs
+++ rust/main.rs
@@ -2,16 +2,10 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let handle = thread::spawn(|| {
-        for i in 1..10 {
-            println!("hi number {} from the spawned thread!", i);
-            thread::sleep(Duration::from_millis(1));
-        }
+    let v = vec![1, 2, 3];
+    let handle = thread::spawn(move || {
+        println!("{:?}", v);
     });
-    for i in 1..5{
-        println!("hi number {} from the main thread!", i);
-        thread::sleep(Duration::from_millis(1));
-    }
 
     handle.join().unwrap();
 }

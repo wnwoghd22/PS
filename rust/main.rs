43924f7bbb1d75241d3037dbecf05d6963a0f49c
Jay <wnwoghd22@gmail.com>
2024-04-21 14:22:02 +0900

    Update main.rs

--- rust/main.rs
+++ rust/main.rs
@@ -1,38 +1,28 @@
-#[derive(Debug)]
-enum List {
-    Cons(i32, RefCell<Rc<List>>),
-    Nil,
-}
-
-impl List {
-    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
-        match self {
-            Cons(_, item) => Some(item),
-            Nil => None,
-        }
-    }
-}
-
-use crate::List::{Cons, Nil};
 use std::cell::RefCell;
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
+
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    parent: RefCell<Weak<Node>>,
+    children: RefCell<Vec<Rc<Node>>>,
+}
 
 fn main() {
-    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
-    println!("count: {}", Rc::strong_count(&a));
-    println!("next: {:?}", a.tail());
+    let leaf = Rc::new(Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
 
-    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
-    println!("a count: {}", Rc::strong_count(&a));
-    println!("b count: {}", Rc::strong_count(&b));
-    println!("{:?}", b.tail());
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
 
-    if let Some(link) = a.tail() {
-        *link.borrow_mut() = Rc::clone(&b);
-    }
-    
-    println!("a count: {}", Rc::strong_count(&a));
-    println!("b count: {}", Rc::strong_count(&b));
+    let branch = Rc::new(Node {
+        value: 5,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![Rc::clone(&leaf)]),
+    });
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
 
-    // println!("next: {:?}", a.tail());
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
 }

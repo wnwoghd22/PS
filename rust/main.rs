69e1374ca18da6f73960d006e37668a8206be2ff
Jay <wnwoghd22@gmail.com>
2024-04-21 15:34:16 +0900

    Update main.rs

--- rust/main.rs
+++ rust/main.rs
@@ -1,11 +1,25 @@
+use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let v = vec![1, 2, 3];
-    let handle = thread::spawn(move || {
-        println!("{:?}", v);
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
     });
 
-    handle.join().unwrap();
+    for received in rx {
+        println!("Got {}", received);
+    }
 }

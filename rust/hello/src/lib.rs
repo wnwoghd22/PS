05661741f13ddc331516f4df896a9cf64f836b93
Jay <wnwoghd22@gmail.com>
2024-04-23 15:50:53 +0900

    Update lib.rs

--- rust/hello/src/lib.rs
+++ rust/hello/src/lib.rs
@@ -1,4 +1,7 @@
-use std::thread;
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread
+};
 
 struct Worker {
     id: usize,
@@ -6,15 +9,25 @@ struct Worker {
 }
 
 impl Worker {
-    pub fn new(id: usize) -> Worker {
-        let thread = thread::spawn(|| {});
+    pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+
+            println!("worker {id} got a job; executing.");
+
+            job();
+        });
+
         Worker { id, thread }
     }
 }
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -27,19 +40,25 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool { workers }
+        ThreadPool { workers, sender }
     }
 
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
 
+        self.sender.send(job).unwrap();
     }
 }

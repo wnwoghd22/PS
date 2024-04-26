c8cb6102d9aea38bcdd70b60d48b7d5195b26139
Jay <wnwoghd22@gmail.com>
2024-04-26 23:56:07 +0900

    Create q2501.rs

---
+++ BOJ/q2501.rs
@@ -0,0 +1,25 @@
+use std::io;
+
+fn main() {
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).unwrap();
+    let values: Vec<u32> = s
+        .as_mut_str()
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
+    let (n, k) = (values[0], values[1]);
+
+    let mut ret = 0;
+    let mut cnt = 0;
+    for p in 1..n {
+        if n % p == 0 {
+            cnt += 1;
+            if cnt == k {
+                ret = p;
+                break;
+            }
+        }
+    }
+    println!("{}", ret);
+}

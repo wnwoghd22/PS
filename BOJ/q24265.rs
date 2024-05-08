3254658cfb9a323214a1fb39e0fbdc996144264a
Jay <wnwoghd22@gmail.com>
2024-05-08 11:26:31 +0900

    Create q24265.rs

---
+++ BOJ/q24265.rs
@@ -0,0 +1,8 @@
+use std::io;
+
+fn main() {
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).unwrap();
+    let n: u64 = s. trim().parse().unwrap();
+    println!("{}\n{}", n * (n - 1) / 2, 2);
+}

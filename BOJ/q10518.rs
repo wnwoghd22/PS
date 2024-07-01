b65e02cda3a6b0848558090707936ecc52068bad
Jay <wnwoghd22@gmail.com>
2024-07-02 01:58:07 +0900

    Update q10518.rs

--- BOJ/q10518.rs
+++ BOJ/q10518.rs
@@ -1,4 +1,5 @@
 use std::io::{self, BufRead};
+use std::mem;
 
 static INF: f64 = 1e17;
 static TOL: f64 = 1e-7;
@@ -8,16 +9,66 @@ struct Pos {
     y: f64,
 }
 
-fn ccw(p1: &Pos, p2: &Pos, p3: &Pos) -> f64 {
+fn cross(p1: &Pos, p2: &Pos, p3: &Pos) -> f64 {
    (p2.x - p1.x) * (p3.y - p2.y) - (p2.y - p1.y) * (p3.x - p2.x)
 }
 
+fn ccw(p1: &Pos, p2: &Pos, p3: &Pos) -> i32 {
+    let c = cross(p1, p2, p3);
+    if c < TOL && c > -TOL { 0 }
+    else if c > 0.0 { 1 }
+    else { -1 }
+}
+
 fn dot(p1: &Pos, p2: &Pos, p3: &Pos) -> f64 {
-    (p2.x - p1.x) * (p3.x - p1.x) + (p2.y - p1.y) * (p3.y - p1.y)
+    (p2.x - p1.x) * (p3.x - p2.x) + (p2.y - p1.y) * (p3.y - p2.y)
+}
+
+fn on_seg_strong(p1: &Pos, p2: &Pos, q1: &Pos) -> bool {
+    let d = dot(p1, q1, p2);
+    d > -TOL && ccw(p1, p2, q1) == 0
+}
+
+fn on_seg_weak(p1: &Pos, p2: &Pos, q1: &Pos) -> bool {
+    let d = dot(p1, q1, p2);
+    d > 0.0 && ccw(p1, p2, q1) == 0
+}
+
+fn inner_check(h: &Vec<Pos>, p: &Pos) -> bool {
+    let mut cnt = 0;
+    for i in 0..h.len() {
+        let mut p1 = &h[i];
+        let mut p2 = &h[(i + 1) % h.len()];
+        if on_seg_strong(p1, p2, p) { return true; }
+        if p1.y - p2.y < TOL && p1.y - p2.y > -TOL { continue; }
+        if p1.y > p2.y { mem::swap(&mut p1, &mut p2); }
+        if p.y + TOL > p2.y || p.y < p1.y { continue; }
+        if ccw(p1, p2, p) > 0 { cnt += 1; }
+    }
+    cnt & 1 == 1
 }
 
 fn intersect(p1: &Pos, p2: &Pos, q1: &Pos, q2: &Pos) -> bool {
-    ccw(p1, p2, q1) * ccw(p1, p2, q2) < TOL && ccw(q1, q2, p1) * ccw(q1, q2, p2) < TOL
+    ccw(p1, p2, q1) * ccw(p1, p2, q2) < 0 && ccw(q1, q2, p1) * ccw(q1, q2, p2) < 0
+}
+
+fn blocked(h: &Vec<Pos>, q1: &Pos, q2: &Pos) -> bool {
+    for i in 0..h.len() {
+        let p1 = &h[i];
+        let p2 = &h[(i + 1) % h.len()];
+        if intersect(p1, p2, q1, q2) || on_seg_weak(q1, q2, p1) || on_seg_weak(q1, q2, p2) {
+            return true;
+        }
+    }
+    false
+}
+
+fn connectable(p1: &Pos, p2: &Pos, inner: &Vec<Pos>, outer: &Vec<Pos>) -> bool {
+    let mid = Pos {
+        x: (p1.x + p2.x) * 0.5,
+        y: (p1.y + p2.y) * 0.5,
+    };
+    !blocked(inner, p1, p2) && !blocked(outer, p1, p2) && inner_check(outer, &mid) && !inner_check(inner, &mid)
 }
 
 fn euc(p1: &Pos, p2: &Pos) -> f64 {
@@ -48,120 +99,36 @@ fn main() {
     for i in 0..n {
         dist[i][(i + 1) % n] = euc(&inner[i], &inner[(i + 1) % n]);
         for j in 0..n {
-            if ccw(&inner[i], &inner[(i + 1) % n], &inner[j]) < 0.0 && 
-                dot(&inner[i], &inner[(i + 1) % n], &inner[j]) > 0.0 {
-                let mut valid = true;
-                for k in 0..n {
-                    let l = (k + 1) % n;
-                    if k == i || k == j || l == i || l == j { continue; }
-                    if intersect(&inner[i], &inner[j], &inner[k], &inner[l]) {
-                        valid = false;
-                        break;
-                    }
-                }
-                for k in 0..m {
-                    let l = (k + 1) % m;
-                    if intersect(&inner[i], &inner[j], &outer[k], &outer[l]) {
-                        valid = false;
-                        break;
-                    }
-                }
-                if valid { dist[i][j] = euc(&inner[i], &inner[j]); }
+            if (ccw(&inner[i], &inner[(i + 1) % n], &inner[j]) > 0 ||
+                on_seg_weak(&inner[i], &inner[j], &inner[(i + 1) % n])) &&
+                connectable(&inner[(i + 1) % n], &inner[j], &inner, &outer) {
+                dist[(i + 1) % n][j] = euc(&inner[j], &inner[(i + 1) % n]);
             }
         }
         for j in 0..m {
-            if ccw(&inner[i], &inner[(i + 1) % n], &outer[j]) < 0.0 && 
-                dot(&inner[i], &inner[(i + 1) % n], &outer[j]) > 0.0 {
-                let mut valid = true;
-                for k in 0..n {
-                    let l = (k + 1) % n;
-                    if k == i || l == i { continue; }
-                    if intersect(&inner[i], &outer[j], &inner[k], &inner[l]) {
-                        valid = false;
-                        break;
-                    }
-                }
-                for k in 0..m {
-                    let l = (k + 1) % m;
-                    if k == j || l == j { continue; }
-                    if intersect(&inner[i], &outer[j], &outer[k], &outer[l]) {
-                        valid = false;
-                        break;
-                    }
-                }
-                if valid { dist[i][j + n] = euc(&inner[i], &outer[j]); }
+            if (ccw(&inner[i], &inner[(i + 1) % n], &outer[j]) > 0 ||
+                on_seg_weak(&inner[i], &outer[j], &inner[(i + 1) % n])) &&
+                connectable(&inner[(i + 1) % n], &outer[j], &inner, &outer) {
+                dist[(i + 1) % n][j + n] = euc(&outer[j], &inner[(i + 1) % n]);
             }
         }
     }
-
     for i in 0..m {
         dist[i + n][(i + 1) % m + n] = euc(&outer[i], &outer[(i + 1) % m]);
         for j in 0..n {
-            if ccw(&outer[(i + m - 1) % m], &outer[i], &inner[j]) < 0.0 {
-                let mut valid = true;
-                for k in 0..n {
-                    let l = (k + 1) % n;
-                    if k == j || l == j { continue; }
-                    if intersect(&outer[i], &inner[j], &inner[k], &inner[l]) {
-                        valid = false;
-                        break;
-                    }
-                }
-                for k in 0..m {
-                    let l = (k + 1) % m;
-                    if k == i || l == i { continue; }
-                    if intersect(&outer[i], &inner[j], &outer[k], &outer[l]) {
-                        valid = false;
-                        break;
-                    }
-                }
-                if valid { dist[i + n][j] = euc(&outer[i], &inner[j]); }
+            if (ccw(&outer[i], &outer[(i + 1) % m], &inner[j]) < 0 ||
+                on_seg_weak(&outer[i], &inner[j], &outer[(i + 1) % m])) &&
+                connectable(&outer[(i + 1) % m], &inner[j], &inner, &outer) {
+                dist[(i + 1) % m + n][j] = euc(&inner[j], &outer[(i + 1) % m]);
             }
         }
         for j in 0..m {
-            if ccw(&outer[(i + m - 1) % m], &outer[i], &outer[j]) < 0.0 && 
-                ccw(&outer[i], &outer[(i + 1) % m], &outer[j]) > 0.0 {
-                let mut valid = true;
-                for k in 0..n {
-                    if intersect(&outer[i], &outer[j], &inner[k], &inner[(k + 1) % n]) {
-                        valid = false;
-                        break;
-                    }
-                }
-                for k in 0..m {
-                    if intersect(&outer[i], &outer[j], &outer[k], &outer[(k + 1) % m]) {
-                        valid = false;
-                        break;
-                    }
-                }
-                let mid = Pos {
-                    x: (outer[i].x + outer[j].x) / 2.0,
-                    y: (outer[i].y + outer[j].y) / 2.0,
-                };
-                let border = Pos {
-                    x: mid.x,
-                    y: 5001f64,
-                };
-
-                let mut cnt = 0;
-                for k in 0..m {
-                    if intersect(&mid, &border, &outer[k], &outer[(k + 1) % m]) {
-                        cnt += 1;
-                    }
-                }
-                if cnt & 1 == 0 { valid = false; }
-
-                if valid { dist[i + n][j + n] = euc(&outer[i], &outer[j]); }
+            if (ccw(&outer[i], &outer[(i + 1) % m], &outer[j]) < 0 ||
+                on_seg_weak(&outer[i], &outer[j], &outer[(i + 1) % m])) &&
+                connectable(&outer[(i + 1) % m], &outer[j], &inner, &outer) {
+                dist[(i + 1) % m + n][j + n] = euc(&outer[j], &outer[(i + 1) % m]);
             }
         }
-    }
-    
-    for i in 0..n+m {
-        for j in 0..n+m {
-            if dist[i][j] > INF - 100.0 { print!("-1 ") }
-            else { print!("{} ", dist[i][j]); }
-        }
-        println!("");
     }
 
     for k in 0..n+m {
@@ -173,11 +140,30 @@ fn main() {
         }
     }
 
-    let mut ret = INF;
-    for i in 0..n+m {
-        for j in 0..n+m {
-            ret = f64::min(ret, dist[i][j] + dist[j][i]);
+    let mut pivot = 0;
+    for i in 1..n {
+        if inner[i].y < inner[pivot].y {
+            pivot = i;
         }
+    }
+
+    let mut hull: Vec<usize> = vec![];
+    let mut origin = pivot;
+    loop {
+        hull.push(origin);
+        let mut x = (origin + 1) % n;
+        for k in 0..n {
+            if ccw(&inner[origin], &inner[x], &inner[k]) < 0 {
+                x = k;
+            }
+        }
+        if x == pivot { break; }
+        origin = x;
+    }
+
+    let mut ret = 0.0;
+    for i in 0..hull.len() {
+        ret += dist[hull[i]][hull[(i + 1) % hull.len()]];
     }
     println!("{}", ret);
 }

d874b6cfdda10b0146fa8b7ab7d80ee807bc7a62
Jay <wnwoghd22@gmail.com>
2024-07-13 23:11:00 +0900

    update

--- ps/q22872.cpp
+++ ps/q22872.cpp
@@ -1,25 +1,67 @@
 #include <iostream>
 
+const int LEN = 1'000'001;
+
+int len, u[LEN], v[LEN];
+
+inline void move(int s, int e) { u[len] = s; v[len] = e; len++; }
+
+void hanoi(int n, int s, int m, int e, int state = -1) {
+	if (!~state) {
+		hanoi(n - 1, s, e, m, 1 << s);
+		move(s, e);
+		hanoi(n - 1, m, s, e, 1 << e);
+		return;
+	}
+	if (n <= 0) return;
+	if (n == 1) {
+		if ((state & 1 << s) && (state & 1 << e)) move(s, m);
+		move(s, e);
+		if (!state) move(m, e); 
+		return;
+	}
+	else if (!state) { // splited
+		if (n == 2) {
+			move(s, e); move(m, e); move(s, e);
+			return;
+		}
+		hanoi(n - 2, s, e, m, 0);
+		move(s, e);
+		hanoi(n - 3, m, e, s, 1 << e);
+		move(m, e); move(m, e);
+		hanoi(n - 2, s, m, e, 1 << m);
+	}
+	else if (!!(state & 1 << s) != !!(state & 1 << e)) { // 1 -> 0, 0 -> 1, normal cost
+		hanoi(n - 2, s, e, m, state);
+		move(s, e); move(s, e);
+		hanoi(n - 2, m, s, e, state);
+	}
+	else if (!(state & 1 << s) && !(state & 1 << e)) { // 0 -> 0, need to split
+		if (n == 2) {
+			move(s, m); move(s, e); move(m, e);
+			return;
+		}
+		hanoi(n - 1, s, e, m, state ^ 1 << s);
+		n % 4 < 2 ? move(s, e) : move(e, s);
+		hanoi(n - 1, m, s, e, state ^ 1 << m);
+	}
+	else if ((state & 1 << s) && (state & 1 << e)) { // 1 -> 1, state chenaged
+		if (n == 2) {
+			move(s, e); move(s, m); move(s, e);
+			return;
+		}
+		hanoi(n - 2, s, m, e, state ^ 14);
+		move(s, m); move(s, m);
+		hanoi(n - 3, e, s, m, state ^ (1 << e));
+		move(s, e);
+		hanoi(n - 2, m, s, e, state ^ (1 << s | 1 << m));
+	}
+}
+
 int main() {
 	int N;
 	std::cin >> N;
-	if (N == 1) {
-		std::cout << 1 << '\n';
-		std::cout << "1 3" << '\n';
-	}
-	else if (N == 2) {
-		std::cout << 3 << '\n';
-		std::cout << "1 2" << '\n';
-		std::cout << "1 3" << '\n';
-		std::cout << "2 3" << '\n';
-	}
-	else if (N == 3) {
-		std::cout << 5 << '\n';
-		std::cout << "1 2" << '\n';
-		std::cout << "1 2" << '\n';
-		std::cout << "1 3" << '\n';
-		std::cout << "2 3" << '\n';
-		std::cout << "2 3" << '\n';
-	}
-	else std::cout << "Everything or Nothing";
+	hanoi(N, 1, 2, 3);
+	std::cout << len << '\n';
+	for (int i = 0; i < len; ++i) std::cout << u[i] << ' ' << v[i] << '\n';
 }
--- ps/q22872_even.cpp
+++ ps/q22872_even.cpp
@@ -4,37 +4,35 @@ const int LEN = 3'000'001;
 
 int len, u[LEN], v[LEN];
 
+inline void move(int s, int e) { u[len] = s; v[len] = e; len++; }
+
 void hanoi(int n, int s, int m, int e, int state = -1) {
 	if (!~state) {
 		hanoi(n - 1, s, e, m, 1 << s);
-		u[len] = s; v[len] = e; len++;
+		move(s, e);
 		hanoi(n - 1, m, s, e, 1 << e);
 		return;
 	}
 	if (n <= 0) return;
 	if (n == 1) {
-		u[len] = s; v[len] = e; len++;
+		move(s, e);
 		return;
 	}
 	if (!!(state & 1 << s) != !!(state & 1 << e)) {
 		hanoi(n - 2, s, e, m, state);
-		u[len] = s; v[len] = e; len++;
-		u[len] = s; v[len] = e; len++;
+		move(s, e); move(s, e);
 		hanoi(n - 2, m, s, e, state);
 	}
 	else if (!(state & 1 << s) && !(state & 1 << e)) {
 		if (n == 2) {
-			u[len] = s; v[len] = m; len++;
-			u[len] = s; v[len] = e; len++;
-			u[len] = m; v[len] = e; len++;
+			move(s, m); move(s, e); move(m, e);
 			return;
 		}
-		// ???
-	}
-	else if (state & 1 << s) {
-	}
-	else if (state & 1 << e) {
-
+		hanoi(n - 2, s, m, e, state);
+		move(s, m); move(s, m);
+		hanoi(n - 2, e, m, s, state);
+		move(m, e); move(m, e);
+		hanoi(n - 2, s, m, e, state);
 	}
 }
 
@@ -44,5 +42,5 @@ int main() {
 	std::cin >> N;
 	hanoi(N, 1, 2, 3);
 	std::cout << len << '\n';
-	// for (int i = 0; i < len; ++i) std::cout << u[i] << ' ' << v[i] << '\n';
+	for (int i = 0; i < len; ++i) std::cout << u[i] << ' ' << v[i] << '\n';
 }

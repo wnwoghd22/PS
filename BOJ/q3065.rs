cc96981cd11701c89dfca38418ccfd17272c2089
Jay <wnwoghd22@gmail.com>
2024-08-06 01:07:08 +0900

    Update q3065.rs

--- BOJ/q3065.rs
+++ BOJ/q3065.rs
@@ -2,10 +2,10 @@ use std::io::{self, BufRead, Write};
 
 fn dfs(u: usize, g: &Vec<Vec<usize>>, d: &mut Vec<usize>, sz: &mut Vec<usize>) {
     sz[u] = 1;
-    for v in &g[u] {
-        d[*v] = d[u] + 1;
-        dfs(*v, g, d, sz);
-        sz[u] += sz[*v];
+    for &v in &g[u] {
+        d[v] = d[u] + 1;
+        dfs(v, g, d, sz);
+        sz[u] += sz[v];
     }
 }
 
@@ -15,16 +15,16 @@ fn main() {
 
     let tc: usize = iter.next().unwrap().unwrap().trim().parse().unwrap();
     for t in 0..tc {
-        if t != 0 { _ = write!(writer, "\n"); }
+        if t != 0 { _ = writeln!(writer); }
         let n: usize = iter.next().unwrap().unwrap().trim().parse().unwrap();
         let n = 1 << n;
-        let mut arr: Vec<usize> = iter.next().unwrap().unwrap()
+        let arr: Vec<usize> = iter.next().unwrap().unwrap()
             .split_whitespace().map(|x| x.parse().unwrap()).collect();
         let queries: Vec<usize> = iter.next().unwrap().unwrap()
             .split_whitespace().map(|x| x.parse().unwrap()).collect();
         let mut v: Vec<usize> = vec![0];
-        while let Some(x) = arr.pop() { v.push(x); }
-        for i in (1..=n).rev() { v.push(i); }
+        v.extend(arr.iter().rev());
+        v.extend((1..=n).rev());
 
         let mut g: Vec<Vec<usize>> = vec![vec![]; n + 1];
         for i in 1..n {
@@ -38,11 +38,11 @@ fn main() {
         let mut sz: Vec<usize> = vec![0; n + 1];
         dfs(v[1], &g, &mut d, &mut sz);
 
-        for u in &queries[1..] {
-            _ = writeln!(writer, "Player {} can be ranked as high as {} or as row as {}.",
-                *u,
-                d[*u] + 1,
-                n - sz[*u] + 1,
+        for &u in &queries[1..] {
+            _ = writeln!(writer, "Player {} can be ranked as high as {} or as low as {}.",
+                u,
+                d[u] + 1,
+                n - sz[u] + 1,
             );
         }
     }

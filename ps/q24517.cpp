8e783b4ce4be5077362ce90112d4a95a391872d8
Jay <wnwoghd22@gmail.com>
2024-08-28 10:02:40 +0900

    Create q24517.cpp

---
+++ ps/q24517.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstring>
+
+int dp[56][1 << 10];
+int Q, A, B, K;
+
+int f(int n, int b) {
+	if (n <= 0) return 0;
+	int& ref = dp[n][b];
+	if (~ref) return ref;
+	ref = 1;
+	for (int i = 0; i < 10; ++i) {
+		if (~b & 1 << i)
+			ref &= f(n - i - 1, b | 1 << i);
+	}
+	return ref ^= 1;
+}
+
+int qry(int a, int b, int k) {
+	int n = b - a;
+	int games = n / k;
+	int s = k * (k + 1) / 2;
+	int ret = f(n % s, 1024 - (1 << k - 1));
+	return ret ^ (games & k & 1);
+}
+
+int main() {
+	std::cin.tie(0)->sync_with_stdio(0);
+	memset(dp, -1, sizeof dp);
+
+	std::cin >> Q;
+	for (int q = 0; q < Q; ++q) {
+		std::cin >> A >> B >> K;
+		std::cout << (qry(A, B, K) ? "swoon" : "raararaara") << '\n';
+	}
+}

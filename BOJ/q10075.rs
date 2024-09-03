ea562f3686734a2ec7186782fc8264c81f8a8938
Jay <wnwoghd22@gmail.com>
2024-09-03 23:40:28 +0900

    Update q10075.rs

--- BOJ/q10075.rs
+++ BOJ/q10075.rs
@@ -8,25 +8,25 @@ fn main() {
         .split_whitespace().map(|x| x.parse().unwrap()).collect();
     let h: Vec<usize> = iter.next().unwrap().unwrap()
         .split_whitespace().map(|x| x.parse().unwrap()).collect();
-    
-    let mut dp: Vec<Vec<i32>> = vec![vec![0, 0]; n];
+
+    let mut dp: Vec<Vec<i32>> = w.iter().map(|&x| vec![0, x]).collect();
     for v in (1..n).rev() {
         let u = h[(v - 1) * 2];
         match h[v * 2 - 1] {
             0 => {
                 dp[u][1] += dp[v][0];
-                dp[u][0] += max(dp[v][0], dp[v][1] + w[v]);
+                dp[u][0] += max(dp[v][0], dp[v][1]);
             },
             1 => {
+                dp[u][1] = max(dp[u][0], dp[u][1]) + max(dp[v][0], dp[v][1]);
                 dp[u][0] += dp[v][0];
-                dp[u][1] += max(dp[v][0], dp[v][1] + w[v]);
             },
             2 => {
+                dp[u][1] = max(dp[u][0] + dp[v][1], dp[u][1] + dp[v][0]);
                 dp[u][0] += dp[v][0];
-                dp[u][1] += dp[v][0];
             },
             _ => (),
         }
     }
-    // TODO: complete
+    println!("{}", max(dp[0][0], dp[0][1]));
 }

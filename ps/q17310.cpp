3c9e924525bc00c2005c458987352e98e6908fac
Jay <wnwoghd22@gmail.com>
2024-11-22 10:32:37 +0900

    Update q17310.cpp

--- ps/q17310.cpp
+++ ps/q17310.cpp
@@ -6,28 +6,29 @@ const int LEN = 1'000'001;
 const ll MOD = 1e9 + 7;
 
 char S[LEN];
-int T, dp[LEN], fail[LEN][21];
+int T, dp[LEN], fail[LEN], p[LEN][21];
 ll solve() {
 	ll ret = 1;
 	std::cin >> S;
 	memset(dp, 0, sizeof dp);
 	memset(fail, 0, sizeof fail);
+	memset(p, 0, sizeof p);
 	for (int i = 1, j = 0, k; S[i]; ++i) {
-		while (j && S[j] ^ S[i]) j = fail[j - 1][0];
+		while (j && S[j] ^ S[i]) j = fail[j - 1];
 		if (S[j] == S[i]) {
-			fail[i][0] = ++j;
-			for (int p = 1; p < 21; ++p)
-				fail[i][p] = fail[fail[i][p - 1] - 1][p - 1];
+			p[i][0] = j;
+			fail[i] = ++j;
+			for (int d = 1; d < 21; ++d)
+				p[i][d] = p[p[i][d - 1]][d - 1];
 		}
 		k = i;
-		for (int p = 20; p >= 0; --p) {
-			if (fail[k][p] >= i / 2)
-				k = fail[k - 1][p];
+		for (int d = 20; d >= 0; --d) {
+			if (p[k][d] + 1 > (i + 1) / 2)
+				k = p[k][d];
 		}
-		ret = ret * (dp[fail[k][0]] + 1) % MOD;
-		dp[i] = dp[fail[i][0]] + 1;
-
-		for (int p = 0; p < 5; ++p) std::cout << fail[i][p] << ' '; std::cout << '\n';
+		if (S[i] == S[p[k][0]]) ret = ret * (dp[p[k][0]] + 2) % MOD;
+		if (S[i] == S[p[i][0]]) dp[i] = dp[p[i][0]] + 1;
+		std::cout << i << ' ' << p[i][0] << ' '  << S[i] << ' ' << S[p[i][0]] << ' ' << fail[i] << ' ' << k << '\n';
 	}
 	for (int i = 0; S[i]; ++i) std::cout << dp[i] << ' ';
 	return ret;

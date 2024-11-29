72ea3be6507c306599013d3ec26f3d6a7e09a04d
Jay <wnwoghd22@gmail.com>
2024-11-30 00:35:21 +0900

    Create q1356.cpp

---
+++ ps/q1356.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+typedef long long ll;
+ll N, len, d[10], l[10], r[10];
+
+int main() {
+	std::cin >> N;
+	while (N) d[len++] = N % 10, N /= 10;
+	l[0] = d[0];
+	for (int i = 1; i < len; ++i) l[i] = l[i - 1] * d[i];
+	r[len - 1] = d[len - 1];
+	for (int i = len - 1; i > 0; --i) r[i - 1] = r[i] * d[i - 1];
+
+	for (int i = 0; i < len; ++i) {
+		std::cout << "i:" << d[i] << ' ' << l[i] << ' ' << r[i] << '\n';
+	}
+	for (int i = 0; i < len - 1; ++i) {
+		if (l[i] == r[i + 1]) {
+			std::cout << "YES";
+			return 0;
+		}
+	}
+	std::cout << "NO";
+}

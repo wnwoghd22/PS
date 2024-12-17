28658caa49388940406d59eccfa1721b20e4bf31
Jay <wnwoghd22@gmail.com>
2024-12-18 01:58:16 +0900

    Create q28227.cpp

---
+++ ps/q28227.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cmath>
+
+typedef long long ll;
+typedef long double ld;
+const int LEN = 200'001;
+const ld PI = 3.1415926535;
+
+ld intersect(const ll& x1, const ll& r1, const ll& x2, const ll& r2) {
+	return (r2 * r2 - x2 * x2 - r1 * r1 + x1 * x1) / (ld)(-2 * x2 + 2 * x1);
+}
+
+int N, X[LEN], R[LEN], st[LEN];
+ld S[LEN], l[LEN], r[LEN];
+
+ld f(ld c, ld x, ld r) {
+	ld y = sqrt(r * r - (x - c) * (x - c));
+	ld fan = r * r * acos(y / r) / 2;
+	ld tri = (c - x) * y / 2;
+	return fan + tri;
+}
+
+int main() {
+	std::cin.tie(0)->sync_with_stdio(0);
+	(std::cout << std::fixed).precision(9);
+	std::cin >> N;
+	for (int i = 0, sp = 0, j; i < N; ++i) {
+		std::cin >> X[i] >> R[i];
+		l[i] = X[i] - R[i]; r[i] = X[i];
+		while (sp) {
+			j = st[sp - 1];
+			if (X[j] <= X[i] - R[i]) break;
+			if (R[i] * R[i] - (X[i] - X[j]) * (X[i] - X[j]) <= R[j] * R[j]) {
+				S[i] += S[j];
+				l[i] = X[j];
+				break;
+			}
+			if ((l[i] = intersect(X[j], R[j], X[i], R[i])) > l[j]) {
+				S[i] += (S[j] -= f(X[j], l[i], R[j]) - f(X[j], r[j], R[j]));
+				r[j] = l[i];
+				break;
+			}	
+			--sp;
+		}
+		st[sp++] = i;
+		S[i] += f(X[i], l[i], R[i]);
+		std::cout << S[i] << '\n';
+	}
+}

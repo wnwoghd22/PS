0c46790c5cc23251b2a69de69e5ec73f640cc315
Jay <wnwoghd22@gmail.com>
2025-01-04 14:59:28 +0900

    Update q23926.cpp

--- ps/q23926.cpp
+++ ps/q23926.cpp
@@ -4,32 +4,27 @@ typedef long long ll;
 
 // need to fix
 ll f(ll x) {
-	ll ret = 0, l = 0, d[20], c[20];
+	if (!x) return 0;
+	ll ret = 0, sum = 0, l = 0, d[20], c[20];
 	while (x) {
 		d[l++] = x % 10;
 		x /= 10;
 	}
-	for (int i = 0, b = 0, a; i < l; ++i) {
+	int b = 0;
+	ret = (d[l - 1] - 1) / 2;
+	b = ~d[l - 1] & 1;
+	for (int i = 1, a; i < l; ++i) {
 		a = d[l - i - 1];
-		c[i] = b ? 4 : (a + 1) / 2;
-		if (~a & (i & 1)) b = 1;
+		ret = ret * 5 + 5 + a / 2;
 	}
-	std::cout << "C: ";
-	for (int i = 0; i < l; ++i) std::cout << c[i] << ' '; std::cout << '\n';
-	
-	std::cout << "D: ";
-	for (int i = 0; i < l; ++i) std::cout << d[i] << ' '; std::cout << '\n';
-
-	for (int i = 0; i < l; ++i) ret = ret * 5 + c[i];
-	return ret;
+	return ret + 1;
 }
 
 int main() {
 	int T;
-	ll l, r;
 	std::cin >> T;
-	while (T--) {
+	for (ll t = 1, l, r; t <= T; ++t) {
 		std::cin >> l >> r;
-		std::cout << f(r) - f(l - 1) << '\n';
+		std::cout << "Case #" << t << ": " << f(r) - f(l - 1) << '\n';
 	}
 }
